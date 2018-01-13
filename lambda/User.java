1df04380e73c757e67c59aa41f8fe04a63f722d5
Jan Bodnar <jan.bodnar@gmail.com>
2018-01-13 15:54:09 +0100

    Create User.java

---
+++ lambda/User.java
@@ -0,0 +1,33 @@
+package com.zetcode;
+
+public class User {
+
+    private String name;
+    private String email;
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "name=" + name + ", email=" + email + '}';
+    }
+}
