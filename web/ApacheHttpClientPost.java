edee8c61015ba334def44a7b4923e841927380e7
Jan Bodnar <jan.bodnar@gmail.com>
2018-01-13 21:13:18 +0100

    Create ApacheHttpClientPost.java

---
+++ web/ApacheHttpClientPost.java
@@ -0,0 +1,37 @@
+package com.zetcode;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+public class ApacheHttpClientPost {
+    
+        public static void main(String[] args) throws IOException {
+        
+        HttpClient client = HttpClientBuilder.create().build();
+        HttpPost request = new HttpPost("https://httpbin.org/post");
+        request.setHeader("User-Agent", "Java client");
+        request.setEntity(new StringEntity("My test data"));
+        
+        HttpResponse response = client.execute(request);
+
+        BufferedReader bufReader = new BufferedReader(new InputStreamReader(
+                response.getEntity().getContent()));
+
+        StringBuilder builder = new StringBuilder();
+        
+        String line;
+        
+        while ((line = bufReader.readLine()) != null) {
+            builder.append(line);
+            builder.append(System.lineSeparator());
+        }
+        
+        System.out.println(builder);
+    }
+}
