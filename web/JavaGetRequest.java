5654010b8ab3365e704d1850473dde79841b9d85
Jan Bodnar <jan.bodnar@gmail.com>
2018-01-13 20:19:08 +0100

    Create JavaGetRequest.java

---
+++ web/JavaGetRequest.java
@@ -0,0 +1,39 @@
+package com.zetcode;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
+public class JavaGetRequest {
+
+    public static void main(String[] args) throws MalformedURLException, 
+            ProtocolException, IOException {
+        
+        String url = "http://www.something.com";
+
+        URL myurl = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) myurl.openConnection();
+
+        con.setRequestMethod("GET");
+
+        StringBuilder content;
+        
+        try (BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()))) {
+            
+            String line;
+            content = new StringBuilder();
+            
+            while ((line = in.readLine()) != null) {
+                content.append(line);
+                content.append(System.lineSeparator());
+            }
+        }
+        
+        System.out.println(content.toString());        
+    }
+}
