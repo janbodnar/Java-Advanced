834c56dedd6af021307f3a649b797a3d4f22fed6
Jan Bodnar <jan.bodnar@gmail.com>
2018-01-13 21:13:00 +0100

    Create ApacheHttpClientGet.java

---
+++ web/ApacheHttpClientGet.java
@@ -0,0 +1,33 @@
+package com.zetcode;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+public class ApacheHttpClientGet {
+
+    public static void main(String[] args) throws IOException {
+        
+        HttpClient client = HttpClientBuilder.create().build();
+        HttpGet request = new HttpGet("http://www.something.com");
+        HttpResponse response = client.execute(request);
+
+        BufferedReader bufReader = new BufferedReader(new InputStreamReader(
+                response.getEntity().getContent()));
+
+        StringBuilder builder = new StringBuilder();
+        
+        String line;
+        
+        while ((line = bufReader.readLine()) != null) {
+            builder.append(line);
+            builder.append(System.lineSeparator());
+        }
+        
+        System.out.println(builder);
+    }
+}
