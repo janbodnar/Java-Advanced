5a902a44560586669de9bf1d4cf25c7f7851949f
Jan Bodnar <jan.bodnar@gmail.com>
2018-01-13 21:14:41 +0100

    Create JavaPostRequest.java

---
+++ web/JavaPostRequest.java
@@ -0,0 +1,50 @@
+package com.zetcode;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+public class JavaPostRequest {
+
+    public static void main(String[] args) throws MalformedURLException,
+            ProtocolException, IOException {
+
+        String url = "https://httpbin.org/post";
+        String urlParameters = "name=Jack&occupation=programmer";
+        byte[] postData = urlParameters.getBytes(StandardCharsets.UTF_8);
+
+        URL myurl = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) myurl.openConnection();
+
+        con.setDoOutput(true);
+        con.setRequestMethod("POST");
+        con.setRequestProperty("User-Agent", "Java client");
+        con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+
+        try (DataOutputStream wr = new DataOutputStream(con.getOutputStream())) {
+            wr.write(postData);
+        }
+
+        StringBuilder content;
+
+        try (BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()))) {
+
+            String line;
+            content = new StringBuilder();
+
+            while ((line = in.readLine()) != null) {
+                content.append(line);
+                content.append(System.lineSeparator());
+            }
+        }
+
+        System.out.println(content.toString());
+    }
+}
