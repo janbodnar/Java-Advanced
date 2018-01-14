18d77fa43a42cb42aa76fa10f7e50ade9f2a5b14
Jan Bodnar <jan.bodnar@gmail.com>
2018-01-14 13:58:57 +0100

    Update ApacheHttpClientGet.java

--- web/ApacheHttpClientGet.java
+++ web/ApacheHttpClientGet.java
@@ -4,30 +4,32 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 
 public class ApacheHttpClientGet {
 
     public static void main(String[] args) throws IOException {
-        
-        HttpClient client = HttpClientBuilder.create().build();
-        HttpGet request = new HttpGet("http://www.something.com");
-        HttpResponse response = client.execute(request);
 
-        BufferedReader bufReader = new BufferedReader(new InputStreamReader(
-                response.getEntity().getContent()));
+        try (CloseableHttpClient client = HttpClientBuilder.create().build()) {
+            HttpGet request = new HttpGet("http://www.something.com");
+            HttpResponse response = client.execute(request);
 
-        StringBuilder builder = new StringBuilder();
-        
-        String line;
-        
-        while ((line = bufReader.readLine()) != null) {
-            builder.append(line);
-            builder.append(System.lineSeparator());
+            BufferedReader bufReader = new BufferedReader(new InputStreamReader(
+                    response.getEntity().getContent()));
+
+            StringBuilder builder = new StringBuilder();
+
+            String line;
+
+            while ((line = bufReader.readLine()) != null) {
+                builder.append(line);
+                builder.append(System.lineSeparator());
+            }
+
+            System.out.println(builder);
         }
-        
-        System.out.println(builder);
     }
 }
+
