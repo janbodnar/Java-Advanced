0fd15c87126e97934ccb1570803c08771975de0c
Jan Bodnar <jan.bodnar@gmail.com>
2018-01-14 21:07:38 +0100

    Create DerbySelectAllCarsEx.java

---
+++ jdbc/DerbySelectAllCarsEx.java
@@ -0,0 +1,64 @@
+package com.zetcode;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class DerbySelectAllCarsEx {
+
+    public static void main(String[] args) {
+        
+        Connection con = null;
+        PreparedStatement pst = null;
+        ResultSet rs = null;
+
+        String url = "jdbc:derby://localhost:1527/testdb";
+        String user = "app";
+        String password = "app";
+
+        try {
+
+            con = DriverManager.getConnection(url, user, password);
+            pst = con.prepareStatement("SELECT * FROM Cars");
+            rs = pst.executeQuery();
+
+            while (rs.next()) {
+
+                System.out.print(rs.getInt(1));
+                System.out.print(": ");
+                System.out.println(rs.getString(2));
+            }
+
+        } catch (SQLException ex) {
+
+            Logger lgr = Logger.getLogger(DerbySelectAllCarsEx.class.getName());
+            lgr.log(Level.SEVERE, ex.getMessage(), ex);
+
+        } finally {
+
+            try {
+
+                if (rs != null) {
+                    rs.close();
+                }
+
+                if (pst != null) {
+                    pst.close();
+                }
+
+                if (con != null) {
+                    con.close();
+                }
+
+            } catch (SQLException ex) {
+
+                Logger lgr = Logger.getLogger(DerbySelectAllCarsEx.class.getName());
+                lgr.log(Level.WARNING, ex.getMessage(), ex);
+            }
+        }
+    }
+}
