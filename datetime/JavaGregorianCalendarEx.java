963943afbe7269e8a281ba0d256b9d7181e60823
Jan Bodnar <jan.bodnar@gmail.com>
2018-01-14 15:07:49 +0100

    Create JavaGregorianCalendarEx.java

---
+++ datetime/JavaGregorianCalendarEx.java
@@ -0,0 +1,33 @@
+package com.zetcode;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class JavaGregorianCalendarEx {
+
+    public static void main(String[] args) {
+
+        String months[] = {"Jan", "Feb", "Mar", "Apr", "May", 
+            "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+
+        // Create a Gregorian calendar initialized
+        // with the current date and time in the
+        // default locale and timezone.
+
+        GregorianCalendar gcal = new GregorianCalendar();
+
+        // Display current time and date information.
+        System.out.printf("Date: %s %d %d%n", months[gcal.get(Calendar.MONTH)],
+                gcal.get(Calendar.DATE), gcal.get(Calendar.YEAR));
+
+        System.out.printf("Time: %d:%d:%d%n",gcal.get(Calendar.HOUR),
+                gcal.get(Calendar.MINUTE), 
+                gcal.get(Calendar.SECOND));
+
+        if (gcal.isLeapYear(gcal.get(Calendar.YEAR))) {
+            System.out.println("The current year is a leap year");
+        } else {
+            System.out.println("The current year is not a leap year");
+        }
+    }
+}
