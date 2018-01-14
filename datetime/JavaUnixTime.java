4e45e2c4343c64beed6c0f7b22715dcb69aaedc3
Jan Bodnar <jan.bodnar@gmail.com>
2018-01-14 14:58:20 +0100

    Create JavaUnixTime.java

---
+++ datetime/JavaUnixTime.java
@@ -0,0 +1,23 @@
+package com.zetcode;
+
+import java.time.Instant;
+
+// Unix time (also known as POSIX time or epoch time)
+// is a system for describing a point in time, defined as the number 
+// of seconds that have elapsed since 00:00:00 Coordinated Universal Time (UTC), 
+// Thursday, 1 January 1970, minus the number of leap seconds 
+// that have taken place since then
+
+public class JavaUnixTime {
+
+    public static void main(String[] args) {
+        
+        long unixTime = System.currentTimeMillis() / 1000L;
+        System.out.println(unixTime);
+        
+        // Java 8
+        
+        long unixTime2 = Instant.now().getEpochSecond();
+        System.out.println(unixTime2);
+    }
+}
