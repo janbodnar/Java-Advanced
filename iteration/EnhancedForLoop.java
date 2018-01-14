2b62975fbe4f273671840770b9f388d6df5510da
Jan Bodnar <jan.bodnar@gmail.com>
2018-01-14 16:26:30 +0100

    Update EnhancedForLoop.java

--- iteration/EnhancedForLoop.java
+++ iteration/EnhancedForLoop.java
@@ -3,6 +3,10 @@ package com.zetcode;
 import java.util.Arrays;
 import java.util.List;
 
+// Java 5 introduced enhanced-for loop. The creation of the iterator and 
+// calls to the hasNext() and next() methods are not explicit, but they 
+// still take place behind the scenes. 
+
 public class EnhancedForLoop {
 
     public static void main(String[] args) {
