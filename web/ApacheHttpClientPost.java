7bfb58d549601020daf2baeb3e0bce2542382277
Jan Bodnar <jan.bodnar@gmail.com>
2018-01-14 14:03:02 +0100

    Update ApacheHttpClientPost.java

--- web/ApacheHttpClientPost.java
+++ web/ApacheHttpClientPost.java
@@ -4,34 +4,37 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 
 public class ApacheHttpClientPost {
-    
-        public static void main(String[] args) throws IOException {
-        
-        HttpClient client = HttpClientBuilder.create().build();
-        HttpPost request = new HttpPost("https://httpbin.org/post");
-        request.setHeader("User-Agent", "Java client");
-        request.setEntity(new StringEntity("My test data"));
-        
-        HttpResponse response = client.execute(request);
 
-        BufferedReader bufReader = new BufferedReader(new InputStreamReader(
-                response.getEntity().getContent()));
+    public static void main(String[] args) throws IOException {
 
-        StringBuilder builder = new StringBuilder();
-        
-        String line;
-        
-        while ((line = bufReader.readLine()) != null) {
-            builder.append(line);
-            builder.append(System.lineSeparator());
+        try (CloseableHttpClient client = HttpClientBuilder.create().build()) {
+
+            HttpPost request = new HttpPost("https://httpbin.org/post");
+            request.setHeader("User-Agent", "Java client");
+            request.setEntity(new StringEntity("My test data"));
+
+            HttpResponse response = client.execute(request);
+
+            BufferedReader bufReader = new BufferedReader(new InputStreamReader(
+                    response.getEntity().getContent()));
+
+            StringBuilder builder = new StringBuilder();
+
+            String line;
+
+            while ((line = bufReader.readLine()) != null) {
+                builder.append(line);
+                builder.append(System.lineSeparator());
+            }
+
+            System.out.println(builder);
         }
-        
-        System.out.println(builder);
     }
 }
+
