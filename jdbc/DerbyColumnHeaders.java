00cbf9ebe1b943768a0bc7461b0d974e978687e9
Jan Bodnar <jan.bodnar@gmail.com>
2018-01-15 16:38:52 +0100

    Create DerbyColumnHeaders.java

---
+++ jdbc/DerbyColumnHeaders.java
@@ -0,0 +1,60 @@
+package com.zetcode;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.util.Formatter;
+
+public class DerbyColumnHeaders {
+
+    public static void main(String[] args) throws SQLException {
+
+        Connection con = null;
+        PreparedStatement pst = null;
+        ResultSet rs = null;
+
+        String url = "jdbc:derby://localhost:1527/testdb";
+        String user = "app";
+        String password = "app";
+
+        try {
+
+            con = DriverManager.getConnection(url, user, password);
+            
+            String query = "SELECT NAME, TITLE From AUTHORS, "
+                    + "Books WHERE AUTHORS.ID=BOOKS.AUTHOR_ID";
+            pst = con.prepareStatement(query);
+
+            rs = pst.executeQuery();
+
+            ResultSetMetaData meta = rs.getMetaData();
+
+            String colname1 = meta.getColumnName(1);
+            String colname2 = meta.getColumnName(2);
+
+            Formatter fmt1 = new Formatter();
+            fmt1.format("%-21s%s", colname1, colname2);
+            System.out.println(fmt1);
+
+            while (rs.next()) {
+                Formatter fmt2 = new Formatter();
+                fmt2.format("%-21s", rs.getString(1));
+                System.out.print(fmt2);
+                System.out.println(rs.getString(2));
+            }
+
+        } finally {
+
+            if (pst != null) {
+                pst.close();
+            }
+
+            if (con != null) {
+                con.close();
+            }
+        }
+    }
+}
