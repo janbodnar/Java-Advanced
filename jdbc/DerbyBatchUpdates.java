c3f15f04b331636429386106d4a3830956b109db
Jan Bodnar <jan.bodnar@gmail.com>
2018-01-16 16:36:48 +0100

    Update DerbyBatchUpdates.java

--- jdbc/DerbyBatchUpdates.java
+++ jdbc/DerbyBatchUpdates.java
@@ -1,66 +1,55 @@
-package com.zetcode;
+package com.zetcode.utils;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class DerbyBatchUpdates {
+public class DBUtils {
 
-    public static void main(String[] args)  {
+    private static final Logger logger = Logger.getLogger(DBUtils.class.getName());
 
-        Connection con = null;
-        Statement st = null;
+    public static void closeResultSet(ResultSet rs) {
 
-        String url = "jdbc:derby://localhost:1527/testdb";
-        String user = "app";
-        String password = "app";
-
-        try {
-
-            con = DriverManager.getConnection(url, user, password);
-
-            con.setAutoCommit(false);
-            st = con.createStatement();
-
-            st.addBatch("DELETE FROM CARS");
-            st.addBatch("INSERT INTO CARS(NAME, PRICE) VALUES('Audi', 52642)");
-            st.addBatch("INSERT INTO CARS(NAME, PRICE) VALUES('Mercedes', 57127)");
-            st.addBatch("INSERT INTO CARS(NAME, PRICE) VALUES('Skoda', 9000)");
-            st.addBatch("INSERT INTO CARS(NAME, PRICE) VALUES('Volvo', 29000)");
-            st.addBatch("INSERT INTO CARS(NAME, PRICE) VALUES('Bentley', 350000)");
-            st.addBatch("INSERT INTO CARS(NAME, PRICE) VALUES('Citroen', 21000)");
-            st.addBatch("INSERT INTO CARS(NAME, PRICE) VALUES('Hummer', 41400)");
-            st.addBatch("INSERT INTO CARS(NAME, PRICE) VALUES('Volkswagen', 21600)");
-            st.addBatch("INSERT INTO CARS(NAME, PRICE) VALUES('Jaguar', 95000)");
-
-            int counts[] = st.executeBatch();
-
-            con.commit();
-
-            System.out.println("Committed " + counts.length + " updates");
-
-        } catch (SQLException ex) {
-            Logger.getLogger(DerbyBatchUpdates.class.getName()).log(Level.SEVERE, null, ex);
-        } finally {
-
+        if (rs != null) {
             try {
-
-                if (st != null) {
-                    st.close();
-                }
-
-                if (con != null) {
-                    con.close();
-                }
-
+                rs.close();
             } catch (SQLException ex) {
+                logger.log(Level.FINEST, "Could not close JDBC ResultSet", ex);
+            } catch (Throwable ex) {
+                // We don't trust the JDBC driver: It might throw RuntimeException or Error.
+                logger.log(Level.FINEST, "Unexpected exception on closing JDBC ResultSet", ex);
+            }
+        }
+    }
+    
+    public static void closeStatement(Statement stmt) {
+        if (stmt != null) {
+            try {
+                stmt.close();
+            } catch (SQLException ex) {
+                logger.log(Level.FINEST, "Could not close JDBC Statement", ex);
+            } catch (Throwable ex) {
+                // We don't trust the JDBC driver: It might throw RuntimeException or Error.
+                logger.log(Level.FINEST, "Unexpected exception on closing JDBC Statement", ex);
+            }
+        }
+    }    
 
-                Logger lgr = Logger.getLogger(DerbyBatchUpdates.class.getName());
-                lgr.log(Level.SEVERE, ex.getMessage(), ex);
+    public static void closeConnection(Connection con) {
+        
+        if (con != null) {
+            try {
+                con.close();
+            } catch (SQLException ex) {
+                logger.log(Level.FINEST, "Could not close JDBC Connection", ex);
+            } catch (Throwable ex) {
+                // We don't trust the JDBC driver: It might throw RuntimeException or Error.
+                logger.log(Level.FINEST, "Unexpected exception on closing JDBC Connection", ex);
             }
         }
     }
 }
+
