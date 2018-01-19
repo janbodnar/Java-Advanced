145ed996d5de5f7688f7ba9955d5d5a2befe4726
Jan Bodnar <jan.bodnar@gmail.com>
2018-01-19 17:35:45 +0100

    Update JavaInputStreamReaderEx2.java

--- inputstreamreader/JavaInputStreamReaderEx2.java
+++ inputstreamreader/JavaInputStreamReaderEx2.java
@@ -3,13 +3,15 @@ package com.zetcode;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 public class JavaInputStreamReaderEx2 {
 
     public static void main(String[] args) throws IOException {
 
         try (BufferedReader bin
-                = new BufferedReader(new InputStreamReader(System.in))) {
+                = new BufferedReader(new InputStreamReader(System.in, 
+                        StandardCharsets.UTF_8))) {
 
             String line;
 
