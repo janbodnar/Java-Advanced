fdb12741c46d6909e3671fe2b04175cabefc8905
Jan Bodnar <jan.bodnar@gmail.com>
2018-01-29 18:46:47 +0100

    Create JavaStreamReduceOptional2.java

---
+++ streams/JavaStreamReduceOptional2.java
@@ -0,0 +1,13 @@
+package com.zetcode;
+
+import java.util.stream.IntStream;
+
+public class JavaStreamReduceOptional2 {
+
+    public static void main(String[] args) {
+
+        IntStream.range(1, 10).reduce((x, y) -> x + y).ifPresent(s -> System.out.println(s));
+        IntStream.range(1, 10).reduce(Integer::sum).ifPresent(s -> System.out.println(s));
+        IntStream.range(1, 10).reduce(MyUtil::add2Ints).ifPresent(s -> System.out.println(s));
+    }
+}
