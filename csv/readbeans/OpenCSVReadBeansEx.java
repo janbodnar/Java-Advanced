14debf70d81166318ba93c66917f0af2f8c9ce4c
Jan Bodnar <jan.bodnar@gmail.com>
2018-01-30 18:29:26 +0100

    Create OpenCSVReadBeansEx.java

---
+++ csv/readbeans/OpenCSVReadBeansEx.java
@@ -0,0 +1,40 @@
+package com.zetcode;
+
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+import com.opencsv.bean.HeaderColumnNameMappingStrategy;
+import com.zetcode.bean.Car;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class OpenCSVReadBeansEx {
+
+    public static void main(String[] args) throws IOException {
+
+        String fileName = "src/main/resources/cars.csv";
+        Path myPath = Paths.get(fileName);
+
+        try (BufferedReader br = Files.newBufferedReader(myPath,
+                StandardCharsets.UTF_8)) {
+
+            HeaderColumnNameMappingStrategy<Car> strategy
+                    = new HeaderColumnNameMappingStrategy<>();
+            strategy.setType(Car.class);
+
+            CsvToBean csvToBean = new CsvToBeanBuilder(br)
+                    .withType(Car.class)
+                    .withMappingStrategy(strategy)
+                    .withIgnoreLeadingWhiteSpace(true)
+                    .build();
+
+            List<Car> cars = csvToBean.parse();
+            
+            cars.forEach(System.out::println);
+        }
+    }
+}
