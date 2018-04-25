5dd4d26222a89680d5b14456c46cfdd384022bf0
Jan Bodnar <jan.bodnar@gmail.com>
2018-04-25 21:13:15 +0200

    Create ArraysSortEx.java

---
+++ comparesort/ArraysSortEx.java
@@ -0,0 +1,31 @@
+package com.zetcode;
+
+import java.util.Arrays;
+import java.util.Random;
+
+// Sorting an array of primitive integer values
+
+public class ArraysSortEx {
+
+    public static void main(String[] args) {
+
+        int[] nums = new int[10];
+        Random rand = new Random();
+
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = rand.nextInt(100) + 1;
+        }
+
+        Arrays.sort(nums);
+
+        System.out.println(Arrays.toString(nums));
+
+        for (int start = 0, end = nums.length - 1; start <= end; start++, end--) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+        }
+
+        System.out.println(Arrays.toString(nums));
+    }
+}
