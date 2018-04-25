8eb9fe27a5725fc76e5b28756864515d179191a5
Jan Bodnar <jan.bodnar@gmail.com>
2018-04-25 12:54:34 +0200

    Create CompareListsApacheCollsEx.java

---
+++ comparesort/CompareListsApacheCollsEx.java
@@ -0,0 +1,33 @@
+package com.zetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.commons.collections4.CollectionUtils;
+
+public class CompareListsApacheCollsEx {
+    
+    public static void main(String[] args) {
+        
+        List<String> one = new ArrayList<>();
+        List<String> two = new ArrayList<>();
+
+        one.add("dog");
+        one.add("pen");
+        one.add("sky");
+        one.add("rock");
+        
+        two.add("dog");
+        two.add("pen");
+        two.add("sky");
+        two.add("rock");
+
+        boolean equal = CollectionUtils.isEqualCollection(one, two);
+        
+        if (equal) {
+            System.out.println("The lists are equal");
+        } else {
+            System.out.println("The lists are not equal");
+        }        
+        
+    }
+}
