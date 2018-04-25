22d62562eefab10dab75c7c5cd159be22098a2b2
Jan Bodnar <jan.bodnar@gmail.com>
2018-04-25 21:09:35 +0200

    Create ArraysSortObjectsEx2.java

---
+++ comparesort/ArraysSortObjectsEx2.java
@@ -0,0 +1,84 @@
+package com.zetcode;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+class Card implements Comparable<Card> {
+    
+    @Override
+    public int compareTo(Card o) {
+        
+        return Comparator.comparing(Card::getValue)
+                .thenComparing(Card::getSuit)
+                .compare(this, o);        
+    }
+    
+    public enum Suits {
+        SPADES,
+        CLUBS,
+        HEARTS,
+        DIAMONDS
+    }
+    
+    public enum Values {
+        TWO,
+        THREE,
+        FOUR,
+        FIVE,
+        SIX,
+        SEVEN,
+        EIGHT,
+        NINE,
+        TEN,
+        JACK,
+        QUEEN,
+        KING,
+        ACE,        
+    }
+    
+    private Suits suit;
+    private Values value;
+    
+    public Card(Values value, Suits suit) {
+        this.value = value;
+        this.suit = suit;
+    }
+    
+    public Values getValue() {
+        return value;
+    }
+    
+    public Suits getSuit() {
+        return suit;
+    }
+    
+    public void showCard() {
+        value = getValue();
+        suit = getSuit();
+        
+        System.out.println(value + " of " + suit);
+    }
+
+    @Override
+    public String toString() {
+        return "Card{" + "suit=" + suit + ", value=" + value + '}';
+    }
+}
+
+public class ArraysSortObjectsEx2 {
+    
+    public static void main(String[] args) {
+        
+        Card[] cards = {
+            new Card(Card.Values.KING, Card.Suits.DIAMONDS),
+            new Card(Card.Values.FIVE, Card.Suits.HEARTS),
+            new Card(Card.Values.ACE, Card.Suits.CLUBS),
+            new Card(Card.Values.NINE, Card.Suits.SPADES),
+            new Card(Card.Values.JACK, Card.Suits.SPADES),
+            new Card(Card.Values.JACK, Card.Suits.DIAMONDS),};
+        
+        Arrays.sort(cards);
+        
+        System.out.println(Arrays.toString(cards));
+    }
+}
