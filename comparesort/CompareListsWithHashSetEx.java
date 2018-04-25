bdf79a2ad463756a387aa7be749f5970294eb6f3
Jan Bodnar <jan.bodnar@gmail.com>
2018-04-25 12:58:11 +0200

    Create CompareListsWithHashSetEx.java

---
+++ comparesort/CompareListsWithHashSetEx.java
@@ -0,0 +1,44 @@
+package com.zetcode;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+// Compare lists by ignoring duplicates
+
+public class CompareListsWithHashSetEx {
+
+    public static void main(String[] args) {
+
+        List<String> words = new ArrayList<>();
+        List<String> words2 = new ArrayList<>();
+
+        words.add("blue");
+        words.add("green");
+        words.add("red");
+        words.add("yellow");
+
+        words2.add("green");
+        words2.add("blue");
+        words2.add("red");
+        words2.add("yellow");     
+        
+        
+        boolean equal = listEqualsIgnoreOrder(words, words2);
+
+        if (equal) {
+            System.out.println("The lists are equal");
+        } else {
+            System.out.println("The lists are not equal");
+        }
+    }
+    
+    private static <T> boolean listEqualsIgnoreOrder(List<T> l1, List<T> l2) {
+        
+        if (l1 == null || l2 == null) {
+            return l1 == l2;
+        }
+             
+        return new HashSet<>(l1).equals(new HashSet<>(l2));
+    }    
+}
