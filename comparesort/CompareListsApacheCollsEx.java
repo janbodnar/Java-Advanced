9cf20304066347e0740972d62ad254f4ef1303e7
Jan Bodnar <jan.bodnar@gmail.com>
2018-04-25 12:59:51 +0200

    Update CompareListsApacheCollsEx.java

--- comparesort/CompareListsApacheCollsEx.java
+++ comparesort/CompareListsApacheCollsEx.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import org.apache.commons.collections4.CollectionUtils;
 
+// Compare lists with Apache Commons library
+
 public class CompareListsApacheCollsEx {
     
     public static void main(String[] args) {
