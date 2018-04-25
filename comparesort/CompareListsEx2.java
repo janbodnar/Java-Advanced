76df76d6e99508ba018dd00081f2310926d1dd00
Jan Bodnar <jan.bodnar@gmail.com>
2018-04-25 12:56:32 +0200

    Create CompareListsEx2.java

---
+++ comparesort/CompareListsEx2.java
@@ -0,0 +1,39 @@
+package com.zetcode;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+// Compare lists by sorting and transfering to strings
+
+public class CompareListsEx2 {
+
+    public static boolean compareList(List<String> l1, List<String> l2) {
+
+        return l1.toString().contentEquals(l2.toString());
+    }
+
+    public static void main(String[] args) {
+
+        List<String> one = new ArrayList<>();
+        List<String> two = new ArrayList<>();
+
+        one.add("dog");
+        one.add("pen");
+        one.add("sky");
+        one.add("rock");
+        
+        two.add("dog");
+        two.add("pen");
+        two.add("rock");
+        two.add("sky");
+        
+        one.sort(Comparator.naturalOrder());
+        two.sort(Comparator.naturalOrder());
+        
+        System.out.println(one);
+        System.out.println(two);
+
+        System.out.println("Output: " + compareList(one, two));
+    }
+}
