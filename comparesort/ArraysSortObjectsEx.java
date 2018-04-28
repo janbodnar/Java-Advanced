0b1c10bf7051b7469464b21d4ffe6b60226243cd
Jan Bodnar <jan.bodnar@gmail.com>
2018-04-28 15:16:21 +0200

    Update ArraysSortObjectsEx.java

--- comparesort/ArraysSortObjectsEx.java
+++ comparesort/ArraysSortObjectsEx.java
@@ -4,22 +4,22 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 class Card implements Comparable<Card> {
-    
+
     @Override
     public int compareTo(Card o) {
-        
+
         return Comparator.comparing(Card::getValue)
                 .thenComparing(Card::getSuit)
-                .compare(this, o);        
+                .compare(this, o);
     }
-    
+
     public enum Suits {
         SPADES,
         CLUBS,
         HEARTS,
         DIAMONDS
     }
-    
+
     public enum Values {
         TWO,
         THREE,
@@ -33,52 +33,57 @@ class Card implements Comparable<Card> {
         JACK,
         QUEEN,
         KING,
-        ACE,        
+        ACE,
     }
-    
+
     private Suits suit;
     private Values value;
-    
+
     public Card(Values value, Suits suit) {
         this.value = value;
         this.suit = suit;
     }
-    
+
     public Values getValue() {
         return value;
     }
-    
+
     public Suits getSuit() {
         return suit;
     }
-    
+
     public void showCard() {
         value = getValue();
         suit = getSuit();
-        
+
         System.out.println(value + " of " + suit);
     }
 
     @Override
     public String toString() {
-        return "Card{" + "suit=" + suit + ", value=" + value + '}';
+        final StringBuilder sb = new StringBuilder("Card{");
+        sb.append("suit=").append(suit);
+        sb.append(", value=").append(value);
+        sb.append('}');
+        return sb.toString();
     }
 }
 
 public class ArraysSortObjectsEx {
-    
+
     public static void main(String[] args) {
-        
+
         Card[] cards = {
-            new Card(Card.Values.KING, Card.Suits.DIAMONDS),
-            new Card(Card.Values.FIVE, Card.Suits.HEARTS),
-            new Card(Card.Values.ACE, Card.Suits.CLUBS),
-            new Card(Card.Values.NINE, Card.Suits.SPADES),
-            new Card(Card.Values.JACK, Card.Suits.SPADES),
-            new Card(Card.Values.JACK, Card.Suits.DIAMONDS),};
-        
-        Arrays.sort(cards);
-        
-        System.out.println(Arrays.toString(cards));
+                new Card(Card.Values.KING, Card.Suits.DIAMONDS),
+                new Card(Card.Values.FIVE, Card.Suits.HEARTS),
+                new Card(Card.Values.ACE, Card.Suits.CLUBS),
+                new Card(Card.Values.NINE, Card.Suits.SPADES),
+                new Card(Card.Values.JACK, Card.Suits.SPADES),
+                new Card(Card.Values.JACK, Card.Suits.DIAMONDS),};
+
+        for (Card card: cards) {
+
+            System.out.println(card);
+        }
     }
 }
