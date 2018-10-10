ebff8d6ef84ccbb385addd52da7a1280a6b1b45f
Jan Bodnar <jan.bodnar@gmail.com>
2018-10-10 08:41:48 +0200

    Create User.java

---
+++ streams/map/User.java
@@ -0,0 +1,28 @@
+package com.zetcode;
+
+public class User {
+
+    private String name;
+    private String occupation;
+
+    public User(String name, String occupation) {
+        this.name = name;
+        this.occupation = occupation;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getOccupation() {
+        return occupation;
+    }
+
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
+    }
+}
