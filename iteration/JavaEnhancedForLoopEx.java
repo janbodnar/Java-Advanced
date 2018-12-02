fad77ed47dd574a6441dd56a26cfccb67aeb4abd
Jan Bodnar <jan.bodnar@gmail.com>
2018-12-02 12:38:19 +0100

    Update and rename JavaEnhancedForLoop.java to JavaEnhancedForLoopEx.java

--- iteration/JavaEnhancedForLoopEx.java
+++ iteration/JavaEnhancedForLoopEx.java
@@ -1,20 +1,19 @@
 package com.zetcode;
 
-import java.util.Arrays;
 import java.util.List;
 
 // Java 5 introduced enhanced-for loop. The creation of the iterator and 
 // calls to the hasNext() and next() methods are not explicit, but they 
 // still take place behind the scenes. 
 
-public class JavaEnhancedForLoop {
+public class JavaEnhancedForLoopEx {
 
     public static void main(String[] args) {
-        
-        List<String> items = Arrays.asList("coin", "ball", "lamp", "spoon");
-        
+
+        var items = List.of("coin", "ball", "lamp", "spoon");
+
         for (String item: items) {
-            
+
             System.out.println(item);
         }
     }
