96e8e9b3ac0e080dc48d9608cc4b77ede6378bad
Jan Bodnar <jan.bodnar@gmail.com>
2018-12-02 11:29:41 +0100

    Update JavaDateCurrentEx.java

--- datetime/JavaDateCurrentEx.java
+++ datetime/JavaDateCurrentEx.java
@@ -9,25 +9,23 @@ public class JavaDateCurrentEx {
 
     public static void main(String[] args) {
 
-        LocalDate date = LocalDate.now();
+        var date = LocalDate.now();
         System.out.println("Current Date: " + date);
 
-        LocalDate date2 = LocalDate.of(2017, Month.NOVEMBER, 13);
+        var date2 = LocalDate.of(2017, Month.NOVEMBER, 13);
         System.out.println("Date from specified date: " + date2);
 
         // current year and month
-        YearMonth yearMo = YearMonth.now();
+        var yearMo = YearMonth.now();
         System.out.println("Current Year and month:" + yearMo);
-        
-        YearMonth specifiedDate = YearMonth.of(2000, Month.NOVEMBER);
-        System.out.println("Specified Year-Month: " + specifiedDate);  
-        
-        MonthDay monthDay = MonthDay.now();
-        System.out.println("Current month and day: " + monthDay);
-        
-        MonthDay specifiedDate2 = MonthDay.of(Month.NOVEMBER, 11);
-        System.out.println("Specified Month-Day: " + specifiedDate2);        
-        
-    }
 
+        var specifiedDate = YearMonth.of(2000, Month.NOVEMBER);
+        System.out.println("Specified Year-Month: " + specifiedDate);
+
+        var monthDay = MonthDay.now();
+        System.out.println("Current month and day: " + monthDay);
+
+        var specifiedDate2 = MonthDay.of(Month.NOVEMBER, 11);
+        System.out.println("Specified Month-Day: " + specifiedDate2);
+    }
 }
