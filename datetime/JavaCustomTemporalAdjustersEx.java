c87a54ab5d2c5a60e958eb1f89119437c99273a3
Jan Bodnar <jan.bodnar@gmail.com>
2018-12-02 11:26:55 +0100

    Create JavaCustomTemporalAdjustersEx.java

---
+++ datetime/JavaCustomTemporalAdjustersEx.java
@@ -0,0 +1,19 @@
+package com.zetcode;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.temporal.TemporalAdjuster;
+
+public class JavaCustomTemporalAdjustersEx {
+
+    public static void main(String[] args) {
+
+        var localDate = LocalDate.of(2018, 12, 1);
+
+        TemporalAdjuster taj = t -> t.plus(Period.ofDays(14));
+        var result = localDate.with(taj);
+
+        System.out.printf("Adding 14 days to %s gives %s", localDate, result);
+
+    }
+}
