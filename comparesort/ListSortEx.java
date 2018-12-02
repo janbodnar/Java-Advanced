d0058efca1480c6818afaf8b63bc484566321951
Jan Bodnar <jan.bodnar@gmail.com>
2018-12-02 16:35:43 +0100

    Update ListSortEx.java

--- comparesort/ListSortEx.java
+++ comparesort/ListSortEx.java
@@ -2,7 +2,6 @@ package com.zetcode;
 
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.List;
 
 // Sorting a list of strings
 
@@ -10,15 +9,15 @@ public class ListSortEx {
 
     public static void main(String[] args) {
 
-        List<String> words = Arrays.asList("dog", "Sun", "Earth", "pen", "plum", "forest", 
+        var words = Arrays.asList("dog", "Sun", "Earth", "pen", "plum", "forest",
                 "atom", "water", "sky", "meadow", "lowland", "swim", "eat", "drum");
-        
+
         words.sort(String.CASE_INSENSITIVE_ORDER);
         System.out.println(words);
-        
+
         words.sort(Comparator.naturalOrder());
         System.out.println(words);
-        
+
         words.sort(Comparator.reverseOrder());
         System.out.println(words);
     }
