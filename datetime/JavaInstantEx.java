a24b05ef49754b7c8ec6089d546ec91866b7b527
Jan Bodnar <jan.bodnar@gmail.com>
2018-12-02 10:46:01 +0100

    Update JavaInstantEx.java

--- datetime/JavaInstantEx.java
+++ datetime/JavaInstantEx.java
@@ -2,26 +2,25 @@ package com.zetcode;
 
 import java.time.Instant;
 import java.time.ZoneId;
-import java.time.ZonedDateTime;
 import java.time.temporal.ChronoUnit;
 
 public class JavaInstantEx {
 
     public static void main(String[] args) {
 
-        Instant timestamp = Instant.now();
+        var timestamp = Instant.now();
         System.out.println("The current timestamp: " + timestamp);
-        
+
         System.out.printf("Unix time: %d%n", timestamp.toEpochMilli());
 
         //Now minus five days
-        Instant minusFive = timestamp.minus(5, ChronoUnit.DAYS);
+        var minusFive = timestamp.minus(5, ChronoUnit.DAYS);
         System.out.println("Now minus five days:" + minusFive);
 
-        ZonedDateTime atZone = timestamp.atZone(ZoneId.of("GMT"));
+        var atZone = timestamp.atZone(ZoneId.of("GMT"));
         System.out.printf("GMT: %s%n", atZone);
 
-        Instant yesterday = Instant.now().minus(24, ChronoUnit.HOURS);
+        var yesterday = Instant.now().minus(24, ChronoUnit.HOURS);
         System.out.println("Yesterday: " + yesterday);
     }
 }
