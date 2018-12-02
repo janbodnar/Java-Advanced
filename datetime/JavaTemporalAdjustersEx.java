0dee2ec2559af6e1a6bd284594817d5e4cbff4ae
Jan Bodnar <jan.bodnar@gmail.com>
2018-12-02 11:19:11 +0100

    Create JavaTemporalAdjustersEx.java

---
+++ datetime/JavaTemporalAdjustersEx.java
@@ -0,0 +1,35 @@
+package com.zetcode;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.temporal.TemporalAdjusters;
+
+// Temporal adjusters are used for modifying temporal objects (LodalDate, LodalDateTime, HijrahDate )
+// They allow to easily calculate calculations such as finding first day of week, moth, year etc.
+
+public class JavaTemporalAdjustersEx {
+
+    public static void main(String[] args) {
+
+        var localDate = LocalDate.now();
+        System.out.printf("today: %s%n", localDate);
+
+        var date1 = localDate.with(TemporalAdjusters.firstDayOfMonth());
+        System.out.printf("first day of month: %s%n", date1);
+
+        var date2 = localDate.with(TemporalAdjusters.lastDayOfMonth());
+        System.out.printf("last day of month: %s%n", date2);
+
+        var date3 = localDate.with(TemporalAdjusters.next(DayOfWeek.MONDAY));
+        System.out.printf("Next monday: %s%n", date3);
+
+        var date4 = localDate.with(TemporalAdjusters.firstDayOfNextMonth());
+        System.out.printf("first day of next month: %s%n", date4);
+
+        var date5 = localDate.with(TemporalAdjusters.lastDayOfYear());
+        System.out.printf("last day of year: %s%n", date5);
+
+        var date6 = localDate.with(TemporalAdjusters.firstDayOfYear());
+        System.out.printf("first day of year: %s%n", date6);
+    }
+}
