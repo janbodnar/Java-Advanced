0029ae8a11d9f4221a6d88caf394010267340518
Jan Bodnar <jan.bodnar@gmail.com>
2018-12-02 12:33:14 +0100

    Update JavaIteratorEx.java

--- iteration/JavaIteratorEx.java
+++ iteration/JavaIteratorEx.java
@@ -1,23 +1,21 @@
 package com.zetcode;
 
-import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
 
-// Iterator brings the Iterator design pattern, which is a common behavioral 
-// pattern used to access the elements of a collection object in sequential 
-// manner without any need to know its underlying representation. 
+// Iterator brings the Iterator design pattern, which is a common behavioral
+// pattern used to access the elements of a collection object in sequential
+// manner without any need to know its underlying representation.
 
 public class JavaIteratorEx {
 
     public static void main(String[] args) {
-        
-        List<String> items = Arrays.asList("coin", "ball", "lamp", "spoon");
-        
-        Iterator it = items.iterator();
-        
+
+        var items = List.of("coin", "ball", "lamp", "spoon");
+
+        var it = items.iterator();
+
         while (it.hasNext()) {
-            
+
             System.out.println(it.next());
         }
     }
