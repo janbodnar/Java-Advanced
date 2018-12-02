0a9cbfedf382af16a319190fe73b9d733543db26
Jan Bodnar <jan.bodnar@gmail.com>
2018-12-02 16:29:01 +0100

    Update ListSortCustomComparatorEx.java

--- comparesort/ListSortCustomComparatorEx.java
+++ comparesort/ListSortCustomComparatorEx.java
@@ -1,8 +1,6 @@
 package com.zetcode;
 
 import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
 
 // Comparing a list of words by their size with a custom comparator
 
@@ -10,20 +8,16 @@ public class ListSortCustomComparatorEx {
 
     public static void main(String[] args) {
 
-        List<String> words = Arrays.asList("pen", "blue", "atom", "to",
+        var words = Arrays.asList("pen", "blue", "atom", "to",
                 "ecclesiastical", "abbey", "car", "ten", "desk", "slim",
                 "journey", "forest", "landscape", "achievement", "Antarctica");
 
-        words.sort((e1, e2) -> {
-            return e1.length() - e2.length();
-        });
+        words.sort((e1, e2) -> e1.length() - e2.length());
 
         words.forEach(System.out::println);
 
-        words.sort((e1, e2) -> {
-            return e2.length() - e1.length();
-        });
-        
+        words.sort((e1, e2) -> e2.length() - e1.length());
+
 //        words.sort(new Comparator<String>() {
 //            @Override
 //            public int compare(String e1, String e2) {
@@ -31,7 +25,7 @@ public class ListSortCustomComparatorEx {
 //
 //            }
 //        });
-        
+
         words.forEach(System.out::println);
     }
 }
