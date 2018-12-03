66fbd16779dcbcffa7c31ea4d896d0ab545947d8
Jan Bodnar <jan.bodnar@gmail.com>
2018-12-03 22:11:20 +0100

    Update and rename JavaCustomTemporalAdjustersEx.java to JavaCustomTemporalAdjusterEx.java

--- datetime/JavaCustomTemporalAdjusterEx.java
+++ datetime/JavaCustomTemporalAdjusterEx.java
@@ -4,7 +4,7 @@ import java.time.LocalDate;
 import java.time.Period;
 import java.time.temporal.TemporalAdjuster;
 
-public class JavaCustomTemporalAdjustersEx {
+public class JavaCustomTemporalAdjusterEx {
 
     public static void main(String[] args) {
 
