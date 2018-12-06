c1b8444612b0b6607d9d9b998ead2aa4ec11043b
Jan Bodnar <jan.bodnar@gmail.com>
2018-12-06 16:41:09 +0100

    Create JavaXmlTransformEx.java

---
+++ xml/xlst/JavaXmlTransformEx.java
@@ -0,0 +1,21 @@
+package com.zetcode;
+
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+import java.io.File;
+
+public class JavaXmlTransformEx {
+
+    public static void main(String[] args) throws TransformerException {
+
+        var xslt = new StreamSource(new File(args[0]));
+        var data = new StreamSource(new File(args[1]));
+
+        var factory = TransformerFactory.newInstance();
+        var transformer = factory.newTransformer(xslt);
+
+        transformer.transform(data, new StreamResult(new File(args[2])));
+    }
+}
