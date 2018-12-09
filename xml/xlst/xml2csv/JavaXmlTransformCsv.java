c913b78e2eb112a41e73d36bba8a9d8c044ae3b5
Jan Bodnar <jan.bodnar@gmail.com>
2018-12-09 10:37:44 +0100

    Create JavaXmlTransformCsv.java

---
+++ xml/xlst/xml2csv/JavaXmlTransformCsv.java
@@ -0,0 +1,26 @@
+package com.zetcode;
+
+
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+import java.io.File;
+
+public class JavaXmlTransformCsv {
+
+    // Program arguments stylesheet.csl users.xml users.csv
+
+    public static void main(String[] args) throws TransformerException {
+
+        var xslt = new StreamSource(new File(args[0]));
+        var data = new StreamSource(new File(args[1]));
+
+        var factory = TransformerFactory.newInstance();
+        var transformer = factory.newTransformer(xslt);
+
+        transformer.transform(data, new StreamResult(new File(args[2])));
+
+        System.out.println("Transformation succeeded");
+    }
+}
