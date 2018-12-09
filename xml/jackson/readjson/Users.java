4117430952f784f0198f33abdd1f125a6c311467
Jan Bodnar <jan.bodnar@gmail.com>
2018-12-09 15:26:41 +0100

    Create Users.java

---
+++ xml/jackson/readjson/Users.java
@@ -0,0 +1,16 @@
+package com.zetcode;
+
+import java.util.List;
+
+public class Users {
+
+    private List<User> users;
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+}
