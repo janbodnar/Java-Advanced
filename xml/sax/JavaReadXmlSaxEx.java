4648e6fcb5274c69132632641eb2c248d8406fcd
Jan Bodnar <jan.bodnar@gmail.com>
2018-12-09 09:09:00 +0100

    Update JavaReadXmlSaxEx.java

--- xml/sax/JavaReadXmlSaxEx.java
+++ xml/sax/JavaReadXmlSaxEx.java
@@ -4,16 +4,15 @@ import org.xml.sax.SAXException;
 
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
-import java.util.List;
 
-public class JavaReadXmlSaxEx  {
+public class JavaReadXmlSaxEx {
 
     public static void main(String[] args)
             throws ParserConfigurationException, SAXException, IOException {
 
-        MyHandler handler = new MyHandler();
-        List<User> lines = handler.parseUsers();
-        
+        var handler = new MyHandler();
+        var lines = handler.parseUsers();
+
         lines.forEach(System.out::println);
     }
 }
