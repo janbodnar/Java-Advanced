96d9f5a42e87e9f85c16e16076e916d4fb33a21e
Jan Bodnar <jan.bodnar@gmail.com>
2018-12-09 11:56:55 +0100

    Update JavaStaxWriteEx.java

--- xml/stax/JavaStaxWriteEx.java
+++ xml/stax/JavaStaxWriteEx.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+// TODO write new user John Doe, gardener
+
 public class JavaStaxWriteEx {
 
     public static void main(String[] args) throws XMLStreamException, IOException {
