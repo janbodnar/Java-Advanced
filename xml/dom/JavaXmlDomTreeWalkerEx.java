eb10495e984f103050eb60e84b680aca83c640bf
Jan Bodnar <jan.bodnar@gmail.com>
2018-12-09 09:32:22 +0100

    Update and rename xml/JavaXmlDomTreeWalkerEx.java to xml/dom/JavaXmlDomTreeWalkerEx.java

--- xml/dom/JavaXmlDomTreeWalkerEx.java
+++ xml/dom/JavaXmlDomTreeWalkerEx.java
@@ -1,39 +1,36 @@
 package com.zetcode;
 
-import java.io.IOException;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.traversal.DocumentTraversal;
 import org.w3c.dom.traversal.NodeFilter;
 import org.w3c.dom.traversal.TreeWalker;
 import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+
 public class JavaXmlDomTreeWalkerEx {
 
-    public static void main(String[] args) throws SAXException, IOException, 
+    public static void main(String[] args) throws SAXException, IOException,
             ParserConfigurationException {
 
-        DocumentBuilderFactory factory
-                = DocumentBuilderFactory.newInstance();
-        DocumentBuilder loader = factory.newDocumentBuilder();
-        Document document = loader.parse("src/main/resources/continents.xml");
+        var factory = DocumentBuilderFactory.newInstance();
+        var loader = factory.newDocumentBuilder();
+        var document = loader.parse("src/main/resources/continents.xml");
 
-        DocumentTraversal traversal = (DocumentTraversal) document;
+        var traversal = (DocumentTraversal) document;
 
-        TreeWalker walker = traversal.createTreeWalker(
+        var walker = traversal.createTreeWalker(
                 document.getDocumentElement(),
                 NodeFilter.SHOW_ELEMENT | NodeFilter.SHOW_TEXT, null, true);
 
         traverseLevel(walker, "");
     }
 
-    private static void traverseLevel(TreeWalker walker,
-            String indent) {
+    private static void traverseLevel(TreeWalker walker, String indent) {
 
-        Node node = walker.getCurrentNode();
+        var node = walker.getCurrentNode();
 
         if (node.getNodeType() == Node.ELEMENT_NODE) {
             System.out.println(indent + node.getNodeName());
@@ -41,7 +38,7 @@ public class JavaXmlDomTreeWalkerEx {
 
         if (node.getNodeType() == Node.TEXT_NODE) {
 
-            String content_trimmed = node.getTextContent().trim();
+            var content_trimmed = node.getTextContent().trim();
 
             if (content_trimmed.length() > 0) {
                 System.out.print(indent);
@@ -49,8 +46,8 @@ public class JavaXmlDomTreeWalkerEx {
             }
         }
 
-        for (Node n = walker.firstChild(); n != null;
-                n = walker.nextSibling()) {
+        for (var n = walker.firstChild(); n != null;
+             n = walker.nextSibling()) {
 
             traverseLevel(walker, indent + "  ");
         }
