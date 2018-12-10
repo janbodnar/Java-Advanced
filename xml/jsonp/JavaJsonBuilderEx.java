dac9cf30343dce8dc1ee4cd461d32dc7d4fcc08c
Jan Bodnar <jan.bodnar@gmail.com>
2018-12-10 08:33:33 +0100

    Create JavaJsonBuilderEx.java

---
+++ xml/jsonp/JavaJsonBuilderEx.java
@@ -0,0 +1,21 @@
+package com.zetcode;
+
+import javax.json.Json;
+import java.time.LocalDate;
+
+public class JavaJsonBuilderEx {
+
+    public static void main(String[] args) {
+
+        var born = LocalDate.of(1992, 3, 2).toString();
+
+        var json = Json.createObjectBuilder()
+                .add("name", "John Doe")
+                .add("occupation", "gardener")
+                .add("born", born).build();
+
+        var result = json.toString();
+
+        System.out.println(result);
+    }
+}
