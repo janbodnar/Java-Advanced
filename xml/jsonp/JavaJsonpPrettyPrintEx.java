aea043c7dc17c46cbb32fa67bad9b49fe78de1fd
Jan Bodnar <jan.bodnar@gmail.com>
2018-12-10 08:30:46 +0100

    Create JavaJsonpPrettyPrintEx.java

---
+++ xml/jsonp/JavaJsonpPrettyPrintEx.java
@@ -0,0 +1,32 @@
+package com.zetcode;
+
+import javax.json.Json;
+import javax.json.stream.JsonGenerator;
+import java.io.StringWriter;
+import java.time.LocalDate;
+import java.util.HashMap;
+
+public class JavaJsonpPrettyPrintEx {
+
+    public static void main(String[] args) {
+
+        var born = LocalDate.of(1992, 3, 2).toString();
+
+        var json = Json.createObjectBuilder()
+                .add("name", "John Doe")
+                .add("occupation", "gardener")
+                .add("born", born).build();
+
+        var config = new HashMap<String, Boolean>();
+        config.put(JsonGenerator.PRETTY_PRINTING, true);
+
+        var jwf = Json.createWriterFactory(config);
+        var sw = new StringWriter();
+
+        try (var jsonWriter = jwf.createWriter(sw)) {
+
+            jsonWriter.writeObject(json);
+            System.out.println(sw.toString());
+        }
+    }
+}
