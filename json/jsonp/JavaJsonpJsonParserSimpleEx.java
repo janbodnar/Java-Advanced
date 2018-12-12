b8b0654657e9ebe0f885fbf76191768aac54cd89
Jan Bodnar <jan.bodnar@gmail.com>
2018-12-12 17:01:21 +0100

    Create JavaJsonpJsonParserSimpleEx.java

---
+++ json/jsonp/JavaJsonpJsonParserSimpleEx.java
@@ -0,0 +1,64 @@
+package com.zetcode;
+
+import javax.json.Json;
+import javax.json.stream.JsonParser;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.nio.charset.StandardCharsets;
+
+public class JavaJsonpJsonParserSimpleEx {
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        var is = new FileInputStream("src/main/resources/users.json");
+
+        var factory = Json.createParserFactory(null);
+        var parser = factory.createParser(is, StandardCharsets.UTF_8);
+
+        if (!parser.hasNext() && parser.next() != JsonParser.Event.START_ARRAY) {
+
+            return;
+        }
+
+        while (parser.hasNext()) {
+
+            var event = parser.next();
+
+            // starting object
+            if (event == JsonParser.Event.START_OBJECT) {
+
+                while (parser.hasNext()) {
+
+                    event = parser.next();
+
+                    if (event == JsonParser.Event.KEY_NAME) {
+
+                        var key = parser.getString();
+
+                        switch (key) {
+
+                            case "name":
+                                parser.next();
+
+                                System.out.printf("Name: %s%n", parser.getString());
+                                break;
+
+                            case "occupation":
+                                parser.next();
+
+
+                                System.out.printf("Occupation: %s%n", parser.getString());
+                                break;
+
+                            case "born":
+                                parser.next();
+
+                                System.out.printf("Born: %s%n%n", parser.getString());
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
