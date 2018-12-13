3ed997fe7942e57a20c0d7c99541ae327be284fb
Jan Bodnar <jan.bodnar@gmail.com>
2018-12-13 21:57:29 +0100

    Update and rename JavaJsonpArrayBuilderEx.java to JsonArrayBuilderEx.java

--- json/jsonp/JsonArrayBuilderEx.java
+++ json/jsonp/JsonArrayBuilderEx.java
@@ -7,26 +7,13 @@ import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.List;
 
-public class JavaJsonpArrayBuilderEx {
+public class JsonArrayBuilderEx {
 
     public static void main(String[] args) {
 
-        var born1 = LocalDate.of(1992, 3, 2);
-        var u1 = new User("John Doe", "gardener", born1);
-
-        var born2 = LocalDate.of(1967, 11, 22);
-        var u2 = new User("Brian Flemming", "teacher", born2);
-
-        var born3 = LocalDate.of(1995, 4, 7);
-        var u3 = new User("Lucy Black", "accountant", born3);
-
-        var born4 = LocalDate.of(1972, 8, 30);
-        var u4 = new User("John Doe", "gardener", born4);
-
-
-        var users = List.of(u1, u2, u3, u4);
-
         var ab = Json.createArrayBuilder();
+
+        var users = createUsers();
 
         users.forEach(user -> {
 
@@ -51,5 +38,22 @@ public class JavaJsonpArrayBuilderEx {
 
             System.out.println(sw);
         }
+    }
+
+    public static List<User> createUsers() {
+
+        var born1 = LocalDate.of(1992, 3, 2);
+        var u1 = new User("John Doe", "gardener", born1);
+
+        var born2 = LocalDate.of(1967, 11, 22);
+        var u2 = new User("Brian Flemming", "teacher", born2);
+
+        var born3 = LocalDate.of(1995, 4, 7);
+        var u3 = new User("Lucy Black", "accountant", born3);
+
+        var born4 = LocalDate.of(1972, 8, 30);
+        var u4 = new User("John Doe", "gardener", born4);
+
+        return List.of(u1, u2, u3, u4);
     }
 }
