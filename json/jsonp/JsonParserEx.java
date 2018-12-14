2c2c2c26aaaa4b3d0e12fcdf87e3b44f106893f5
Jan Bodnar <jan.bodnar@gmail.com>
2018-12-14 11:59:34 +0100

    Update and rename JavaJsonpJsonParserEx.java to JsonParserEx.java

--- json/jsonp/JsonParserEx.java
+++ json/jsonp/JsonParserEx.java
@@ -5,7 +5,7 @@ import javax.json.stream.JsonParser;
 import java.io.IOException;
 import java.net.URL;
 
-public class JavaJsonpJsonParserEx {
+public class JsonParserEx {
 
     public static void main(String[] args) throws IOException {
 
