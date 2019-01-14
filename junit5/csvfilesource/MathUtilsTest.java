7f84a1c9f5ef6fd038f4f928760c8836162a450d
Jan Bodnar <jan.bodnar@gmail.com>
2019-01-14 22:32:47 +0100

    Update MathUtilsTest.java

--- junit5/csvfilesource/MathUtilsTest.java
+++ junit5/csvfilesource/MathUtilsTest.java
@@ -1,5 +1,6 @@
 package com.zetcode.utils;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
@@ -7,7 +8,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MathUtilsTest {
 
-    @ParameterizedTest
+    @DisplayName("should calculate the correct sum")
+    @ParameterizedTest(name = "{index} => x={0}, y={1}, z={2} sum={3}")
     @CsvFileSource(resources = "/test-data.csv", numLinesToSkip = 1)
     void sum(int x, int y, int z, int sum) {
 
