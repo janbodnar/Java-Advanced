6c8b394446651ada50a93a8c61bb6ee879745cd6
Jan Bodnar <jan.bodnar@gmail.com>
2019-02-14 15:02:08 +0100

    Update ProcessBuilderEx.java

--- inputstreamreader/ProcessBuilderEx.java
+++ inputstreamreader/ProcessBuilderEx.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 
 public class ProcessBuilderEx {
 
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws IOException {
 
         var processBuilder = new ProcessBuilder();
 
@@ -14,21 +14,14 @@ public class ProcessBuilderEx {
 
         var process = processBuilder.start();
 
-        var reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        try (var reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
 
-        String line;
+            String line;
 
-        while ((line = reader.readLine()) != null) {
-            System.out.println(line);
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+
         }
-
     }
 }
-
-   February 2019      
-Su Mo Tu We Th Fr Sa  
-                1  2  
- 3  4  5  6  7  8  9  
-10 11 12 13 14 15 16  
-17 18 19 20 21 22 23  
-24 25 26 27 28        
