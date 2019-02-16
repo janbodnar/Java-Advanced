18f63d22c932a5ec14b6a387a1ce538897bf5ce2
Jan Bodnar <jan.bodnar@gmail.com>
2019-02-16 13:35:28 +0100

    Create MultilineString.java

---
+++ strings/MultilineString.java
@@ -0,0 +1,15 @@
+package com.zetcode;
+
+public class MultilineString {
+
+    static String lyrics = "I cheated myself\n" +
+"like I knew I would\n" +
+"I told ya, I was trouble\n" +
+"you know that I'm no good";
+    
+    
+    public static void main(String[] args) {
+        
+        System.out.println(lyrics);
+    }
+}
