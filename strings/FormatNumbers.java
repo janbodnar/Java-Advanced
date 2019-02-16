b46b91cdc9e989302f991ecc21f126c68ec6f10f
Jan Bodnar <jan.bodnar@gmail.com>
2019-02-16 13:27:23 +0100

    Create FormatNumbers.java

---
+++ strings/FormatNumbers.java
@@ -0,0 +1,13 @@
+package com.zetcode;
+
+public class FormatNumbers {
+
+    public static void main(String[] args) {
+        
+        System.out.format("%d%n", 12263);
+        System.out.format("%o%n", 12263);
+        System.out.format("%x%n", 12263);
+        System.out.format("%e%n", 0.03452342263);
+        System.out.format("%d%%%n", 45);    
+    }
+}
