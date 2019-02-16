497fb2934de3959ef8bd8a00dfa24db35a093165
Jan Bodnar <jan.bodnar@gmail.com>
2019-02-16 13:52:52 +0100

    Update FilesListExtension.java

--- io/FilesListExtension.java
+++ io/FilesListExtension.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-public class FileListExtension {
+public class FilesListExtension {
 
     public static void main(String[] args) throws IOException {
 
