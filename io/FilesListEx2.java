a62c1b4dc4b019654d838211cea978088a2a63e8
Jan Bodnar <jan.bodnar@gmail.com>
2019-02-16 13:43:28 +0100

    Create FilesListEx2.java

---
+++ io/FilesListEx2.java
@@ -0,0 +1,17 @@
+package com.zetcode;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+public class FilesListEx2 {
+
+    public static void main(String[] args) throws IOException {
+
+        var homeDir = System.getProperty("user.home");
+
+        Files.list(new File(homeDir).toPath())
+                .filter(path -> path.toFile().isDirectory())
+                .forEach(System.out::println);
+    }
+}
