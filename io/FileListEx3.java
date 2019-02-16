0ffe59f54f9d842c01842eae912fc7d7ddd28533
Jan Bodnar <jan.bodnar@gmail.com>
2019-02-16 13:43:42 +0100

    Create FileListEx3.java

---
+++ io/FileListEx3.java
@@ -0,0 +1,17 @@
+package com.zetcode;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class FileListEx3 {
+
+    public static void main(String[] args) throws IOException {
+
+        var homeDir = System.getProperty("user.home")
+                + System.getProperty("file.separator") + "Downloads";
+
+        Files.list(Paths.get(homeDir)).filter(path -> path.toString().endsWith(".pdf"))
+                .forEach(System.out::println);
+    }
+}
