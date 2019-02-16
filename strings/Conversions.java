0a07cc0b1b1a7f11b813cfbdb5153e646d0ed0f9
Jan Bodnar <jan.bodnar@gmail.com>
2019-02-16 13:31:18 +0100

    Create Conversions.java

---
+++ strings/Conversions.java
@@ -0,0 +1,10 @@
+package com.zetcode;
+
+public class Conversions {
+
+    public static void main(String[] args) {
+        
+        System.out.format("There are %d %s.%n", 5, "pencils");
+        System.out.printf("The rock weighs %f kilograms.%n", 5.345);    
+    }
+}
