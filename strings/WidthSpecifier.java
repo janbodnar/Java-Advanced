90b6758892aa13206c82f8d37846995670898b43
Jan Bodnar <jan.bodnar@gmail.com>
2019-02-16 13:28:20 +0100

    Create WidthSpecifier.java

---
+++ strings/WidthSpecifier.java
@@ -0,0 +1,19 @@
+package com.zetcode;
+
+public class WidthSpecifier {
+
+    public static void main(String[] args) {
+        
+        System.out.println(1);
+        System.out.println(16);
+        System.out.println(1655);
+        System.out.println(16567);
+        System.out.println(166701);
+        
+        System.out.format("%10d%n", 1);
+        System.out.format("%10d%n", 16);
+        System.out.format("%10d%n", 1655);
+        System.out.format("%10d%n", 16567);
+        System.out.format("%10d%n", 166701);                 
+    }
+}
