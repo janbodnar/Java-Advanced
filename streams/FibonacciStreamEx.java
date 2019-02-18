c472e21e5b7867cd48f59d278a88cc17ec3250c4
Jan Bodnar <jan.bodnar@gmail.com>
2019-02-18 17:57:14 +0100

    Create FibonacciStreamEx.java

---
+++ streams/FibonacciStreamEx.java
@@ -0,0 +1,25 @@
+package com.zetcode;
+
+import java.math.BigInteger;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class FibonacciStreamEx {
+
+    public static List<BigInteger> fibonacci(int limit) {
+
+        var vals = Stream.iterate(new BigInteger[] { BigInteger.ZERO, BigInteger.ONE },
+                t -> new BigInteger[]{t[1], t[0].add(t[1])})
+                .limit(limit)
+                .map(n -> n[1])
+                .collect(Collectors.toList());
+                
+        return vals;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(fibonacci(99));
+    }
+}
