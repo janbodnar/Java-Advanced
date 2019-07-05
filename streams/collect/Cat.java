b59f84fcee882300c86c48a7c09c1eb5eae02593
Jan Bodnar <jan.bodnar@gmail.com>
2019-07-05 14:04:45 +0200

    Create Cat.java

---
+++ streams/collect/Cat.java
@@ -0,0 +1,38 @@
+package com.zetcode;
+
+public class Cat {
+
+    private String name;
+    private int age;
+
+    public Cat(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+
+        final StringBuilder sb = new StringBuilder("Cat{");
+        sb.append("name='").append(name).append('\'');
+        sb.append(", age=").append(age);
+        sb.append('}');
+        return sb.toString();
+    }
+}
