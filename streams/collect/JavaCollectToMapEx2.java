7a93391edec96f7e1d87c2e4c38413f4c054421d
Jan Bodnar <jan.bodnar@gmail.com>
2019-07-05 15:36:18 +0200

    Create JavaCollectToMapEx2.java

---
+++ streams/collect/JavaCollectToMapEx2.java
@@ -0,0 +1,21 @@
+package com.zetcode;
+
+import java.nio.CharBuffer;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class JavaCollectToMapEx2 {
+
+    public static void main(String[] args) {
+    
+        char[] letters = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+
+        Map<Integer, Character> asciiMap = CharBuffer.wrap(letters)
+                .chars()
+                .mapToObj(e -> (char) e)
+                .collect(Collectors.toMap(Object::hashCode, Function.identity()));
+
+        System.out.println(asciiMap);
+    }
+}
