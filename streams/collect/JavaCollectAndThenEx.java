3096b8fca2249de4fe1566880434ab9e370d032b
Jan Bodnar <jan.bodnar@gmail.com>
2019-07-05 23:28:59 +0200

    Update JavaCollectAndThenEx.java

--- streams/collect/JavaCollectAndThenEx.java
+++ streams/collect/JavaCollectAndThenEx.java
@@ -5,6 +5,8 @@ import java.util.List;
 import java.util.Locale;
 import java.util.stream.Collectors;
 
+// calculate an average price and then 
+// format it
 public class JavaCollectAndThenEx {
 
     public static void main(String[] args) {
