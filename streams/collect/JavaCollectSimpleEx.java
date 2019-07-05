00f7126995690d114b22d6e49015e4d2ced681f5
Jan Bodnar <jan.bodnar@gmail.com>
2019-07-05 13:25:53 +0200

    Create JavaCollectSimpleEx.java

---
+++ streams/collect/JavaCollectSimpleEx.java
@@ -0,0 +1,19 @@
+package com.zetcode;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class JavaCollectSimpleEx {
+
+    public static void main(String[] args) {
+
+        var words = List.of("marble", "coin", "forest", "falcon",
+                "sky", "cloud", "eagle", "lion");
+
+        // filter all four character words into a list
+        var words4 = words.stream().filter(word -> word.length() == 4)
+                .collect(Collectors.toList());
+
+        System.out.println(words4);
+    }
+}
