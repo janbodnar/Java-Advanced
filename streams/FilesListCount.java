546d58dd70ff1dbea7346495d25d805589899e62
Jan Bodnar <jan.bodnar@gmail.com>
2019-07-07 18:28:01 +0200

    Update FilesListCount.java

--- streams/FilesListCount.java
+++ streams/FilesListCount.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+// count # of PDF files in the Dowloads directory
 public class FilesListCount {
 
     public static void main(String[] args) throws IOException {
