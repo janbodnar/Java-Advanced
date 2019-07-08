46a87cb0f299ae719930c5e8aeab09462002fe75
Jan Bodnar <jan.bodnar@gmail.com>
2019-07-08 15:01:32 +0200

    Create DatagramSocketEx.java

---
+++ net/socket/DatagramSocketEx.java
@@ -0,0 +1,33 @@
+package com.zetcode;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+// DatagramSocket provides network communication via UDP protocol
+
+public class DatagramSocketEx {
+
+    public static void main(String[] args) throws IOException {
+
+        var hostname = "djxmmx.net";
+        int port = 17;
+
+        var address = InetAddress.getByName(hostname);
+
+        try (var socket = new DatagramSocket()) {
+
+            var request = new DatagramPacket(new byte[1], 1, address, port);
+            socket.send(request);
+
+            var buffer = new byte[512];
+            var response = new DatagramPacket(buffer, buffer.length);
+            socket.receive(response);
+
+            var quote = new String(buffer, 0, response.getLength());
+
+            System.out.println(quote);
+        }
+    }
+}
