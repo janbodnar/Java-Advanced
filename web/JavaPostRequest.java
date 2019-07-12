4178b2d124b5ce0eb7223b36e3c1c9d6e6fe9dd4
Jan Bodnar <jan.bodnar@gmail.com>
2019-07-12 09:56:50 +0200

    Update JavaPostRequest.java

--- web/JavaPostRequest.java
+++ web/JavaPostRequest.java
@@ -5,8 +5,6 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 
@@ -14,16 +12,15 @@ public class JavaPostRequest {
 
     private static HttpURLConnection con;
 
-    public static void main(String[] args) throws MalformedURLException,
-            ProtocolException, IOException {
+    public static void main(String[] args) throws IOException {
 
-        String url = "https://httpbin.org/post";
-        String urlParameters = "name=Jack&occupation=programmer";
+        var url = "https://httpbin.org/post";
+        var urlParameters = "name=Jack&occupation=programmer";
         byte[] postData = urlParameters.getBytes(StandardCharsets.UTF_8);
 
         try {
 
-            URL myurl = new URL(url);
+            var myurl = new URL(url);
             con = (HttpURLConnection) myurl.openConnection();
 
             con.setDoOutput(true);
@@ -31,19 +28,20 @@ public class JavaPostRequest {
             con.setRequestProperty("User-Agent", "Java client");
             con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
 
-            try (DataOutputStream wr = new DataOutputStream(con.getOutputStream())) {
+            try (var wr = new DataOutputStream(con.getOutputStream())) {
+                
                 wr.write(postData);
             }
 
             StringBuilder content;
 
-            try (BufferedReader in = new BufferedReader(
+            try (var br = new BufferedReader(
                     new InputStreamReader(con.getInputStream()))) {
 
                 String line;
                 content = new StringBuilder();
 
-                while ((line = in.readLine()) != null) {
+                while ((line = br.readLine()) != null) {
                     content.append(line);
                     content.append(System.lineSeparator());
                 }
@@ -52,7 +50,7 @@ public class JavaPostRequest {
             System.out.println(content.toString());
 
         } finally {
-            
+
             con.disconnect();
         }
     }
