6c2ce49b135d577cb3757be90ee098d258c9bbad
Jan Bodnar <jan.bodnar@gmail.com>
2019-07-12 17:28:15 +0200

    Create OptionalMapOf.java

---
+++ optional/OptionalMapOf.java
@@ -0,0 +1,22 @@
+package com.zetcode;
+
+import java.util.Optional;
+import java.util.function.Function;
+
+public class OptionalMapOf {
+
+    public static void main(String[] args) {
+
+        Function<String, Optional<String>> upperCase
+                = s -> (s == null) ? Optional.empty() : Optional.of(s.toUpperCase());
+
+        Optional<String> word = Optional.of("falcon");
+
+        Optional<Optional<String>> opt1 = word.map(upperCase);
+        Optional<String> opt2 = word.flatMap(upperCase);
+
+        opt1.ifPresent(s -> s.ifPresent(System.out::println));
+        opt2.ifPresentOrElse(System.out::println,
+                () -> System.out.println("Invalid value"));
+    }
+}
