1d13a74f6a706e31e874f4ee3cbb08c08c0c04c3
Jan Bodnar <jan.bodnar@gmail.com>
2019-07-13 11:39:06 +0200

    Create HashMapLiteralInit.java

---
+++ collections/hashmap/HashMapLiteralInit.java
@@ -0,0 +1,30 @@
+package com.zetcode;
+
+import java.util.Map;
+
+import static java.util.Map.entry;
+
+// factory initialization methods since Java 9
+public class HashMapLiteralInit {
+
+    public static void main(String[] args) {
+
+        // up to 10 elements:
+        Map<Integer, String> colours = Map.of(
+                1, "red",
+                2, "blue",
+                3, "brown"
+        );
+
+        System.out.println(colours);
+
+        // any number of elements
+        Map<String, String> countries = Map.ofEntries(
+                entry("de", "Germany"),
+                entry("sk", "Slovakia"),
+                entry("ru", "Russia")
+        );
+
+        System.out.println(countries);
+    }
+}
