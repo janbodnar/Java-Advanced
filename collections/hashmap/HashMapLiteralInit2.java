11902b47384d70f32c629bb792a4af91db93fd3b
Jan Bodnar <jan.bodnar@gmail.com>
2019-07-13 11:42:24 +0200

    Create HashMapLiteralInit2.java

---
+++ collections/hashmap/HashMapLiteralInit2.java
@@ -0,0 +1,19 @@
+package com.zetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// up to Java 8
+public class HashMapLiteralInit2 {
+
+    public static void main(String[] args) {
+
+        Map<String, String> countries = new HashMap<>() {{
+            put("de", "Germany");
+            put("sk", "Slovakia");
+            put("ru", "Russia");
+        }};
+
+        System.out.println(countries);
+    }
+}
