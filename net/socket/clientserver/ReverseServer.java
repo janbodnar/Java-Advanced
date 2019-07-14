c688b8f7e9220c7f9662669499f8acad0fd0b75f
Jan Bodnar <jan.bodnar@gmail.com>
2019-07-14 12:44:25 +0200

    Create ReverseServer.java

---
+++ net/socket/clientserver/ReverseServer.java
@@ -0,0 +1,49 @@
+package com.zetcode;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+
+public class ReverseServer {
+
+    public static void main(String[] args) throws IOException {
+
+        int port = 8081;
+
+        try (var serverSocket = new ServerSocket(port)) {
+
+            System.out.println("Server is listening on port " + port);
+
+            while (true) {
+
+                try (var socket = serverSocket.accept()) {
+
+                    System.out.println("client connected");
+
+                    try (var reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                         var writer = new PrintWriter(socket.getOutputStream(), true)) {
+
+                        String text;
+
+                        do {
+
+                            text = reader.readLine();
+
+                            if (text != null) {
+
+                                var reversed = new StringBuilder(text).reverse().toString();
+                                writer.println("Server: " + reversed);
+
+                                System.out.println(text);
+                            }
+                        } while (!"bye".equals(text));
+
+                        System.out.println("client disconnected");
+                    }
+                }
+            }
+        }
+    }
+}
