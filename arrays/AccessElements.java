7b7b0a509d19c3e57b84f69ebe98730f1fdc4bf4
Jan Bodnar <jan.bodnar@gmail.com>
2019-07-26 16:14:18 +0200

    Create AccessElements.java

---
+++ arrays/AccessElements.java
@@ -0,0 +1,14 @@
+package com.zetcode;
+
+public class AccessElements {
+
+    public static void main(String[] args) {
+
+        String[] names = {"Jane", "Thomas", "Lucy", "David"};
+
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
+    }
+}
