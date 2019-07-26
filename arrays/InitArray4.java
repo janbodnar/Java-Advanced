1f685815361f5ebcf5c2018005c2b7af279f050a
Jan Bodnar <jan.bodnar@gmail.com>
2019-07-26 17:51:16 +0200

    Create InitArray4.java

---
+++ arrays/InitArray4.java
@@ -0,0 +1,21 @@
+package com.zetcode;
+
+import java.util.Arrays;
+
+public class InitArray4 {
+
+    public static void main(String[] args) {
+
+        int[] a = new int[10];
+
+        Arrays.fill(a, 1);
+
+        System.out.println(Arrays.toString(a));
+
+        int[] b = new int[10];
+
+        Arrays.setAll(b, e -> e++);
+
+        System.out.println(Arrays.toString(b));
+    }
+}
