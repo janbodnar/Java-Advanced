a002ae0f15ee3cde189611c6bc83d38aa9e8214b
Jan Bodnar <jan.bodnar@gmail.com>
2019-07-26 13:51:06 +0200

    Update JavaStatsEx.java

--- apps/commandline/JavaStatsEx.java
+++ apps/commandline/JavaStatsEx.java
@@ -2,20 +2,20 @@ package com.zetcode;
 
 /**
  * Starter class for MyStats application.
- * 
+ *
  * @author janbodnar
  */
 
 public class JavaStatsEx {
-    
+
     /**
      * Application entry point.
-     * 
+     *
      * @param args application command line arguments
      */
     public static void main(String[] args) {
-        
-        MyStatsApp msp = new MyStatsApp();
-        msp.run(args);
+
+        var app = new MyStatsApp();
+        app.run(args);
     }
 }
