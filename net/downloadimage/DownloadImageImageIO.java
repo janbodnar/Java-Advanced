bd96c15115abf72e929329257a315cf6370bf0fa
Jan Bodnar <jan.bodnar@gmail.com>
2019-07-26 19:18:51 +0200

    Create DownloadImageImageIO.java

---
+++ net/downloadimage/DownloadImageImageIO.java
@@ -0,0 +1,30 @@
+package com.zetcode;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class DownloadImageImageIO {
+
+    private static HttpURLConnection con;
+
+    public static void main(String[] args) throws IOException {
+
+        var url = "http://httpbin.org/image/jpeg";
+
+        try {
+            var myurl = new URL(url);
+            con = (HttpURLConnection) myurl.openConnection();
+
+            BufferedImage image = ImageIO.read(con.getInputStream());
+            ImageIO.write(image, "jpg", new File("src/resources/jackal.jpg"));
+
+        } finally {
+
+            con.disconnect();
+        }
+    }
+}
