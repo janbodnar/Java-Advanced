cd57a86ee6f96e01d1a099abd5d13f29bace4434
Jan Bodnar <jan.bodnar@gmail.com>
2019-07-26 17:45:22 +0200

    Update ArrayCopy.java

--- arrays/ArrayCopy.java
+++ arrays/ArrayCopy.java
@@ -11,8 +11,13 @@ public class ArrayCopy {
         int[] vals2 = Arrays.copyOf(vals, 8);
         int[] vals3 = Arrays.copyOfRange(vals, 2, 7);
 
+        // we need to create an empty array
+        int[] vals4 = new int[8];
+        System.arraycopy(vals, 0, vals4, 0, vals.length);
+
         System.out.println(Arrays.toString(vals));
         System.out.println(Arrays.toString(vals2));
         System.out.println(Arrays.toString(vals3));
+        System.out.println(Arrays.toString(vals4));
     }
 }
