dd33f0e828c65b565bc0f4119c122565a41e27b3
Jan Bodnar <jan.bodnar@gmail.com>
2019-07-29 16:07:35 +0200

    Update JavaCopyFile.java

--- io/file/JavaCopyFile.java
+++ io/file/JavaCopyFile.java
@@ -9,8 +9,8 @@ public class JavaCopyFile {
 
     public static void main(String[] args) throws IOException {
 
-        File source = new File("src/resources/bugs.txt");
-        File dest = new File("src/resources/bugs2.txt");
+        var source = new File("src/resources/bugs.txt");
+        var dest = new File("src/resources/bugs2.txt");
 
         Files.copy(source.toPath(), dest.toPath(), 
                 StandardCopyOption.REPLACE_EXISTING);
