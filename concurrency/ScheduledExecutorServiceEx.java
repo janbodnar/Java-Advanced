95ecd90849151fa7015154bac39f5e8aae49d5df
Jan Bodnar <jan.bodnar@gmail.com>
2019-08-01 14:21:25 +0200

    Create ScheduledExecutorServiceEx.java

---
+++ concurrency/ScheduledExecutorServiceEx.java
@@ -0,0 +1,32 @@
+package com.zetcode;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+class Task implements Runnable {
+
+    public void run() {
+
+        System.out.println("Doing task");
+    }
+}
+
+public class ScheduledExecutorServiceEx {
+
+    public static void main(final String[] arguments)  {
+
+        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+
+        ScheduledFuture<?> taskHandler =
+                scheduler.scheduleAtFixedRate(new Task(), 2, 2, TimeUnit.SECONDS);
+
+        scheduler.schedule(() -> {
+            taskHandler.cancel(true);
+            scheduler.shutdown();
+        }, 10, TimeUnit.SECONDS);
+
+        System.out.println("Launching tasks");
+    }
+}
