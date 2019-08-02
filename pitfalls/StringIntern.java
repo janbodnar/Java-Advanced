ad94be54ce3bfbc81315b5370c93375602a0389a
Jan Bodnar <jan.bodnar@gmail.com>
2019-08-02 16:59:53 +0200

    Create StringIntern.java

---
+++ pitfalls/StringIntern.java
@@ -0,0 +1,20 @@
+package com.zetcode;
+
+// the == operator compares object references
+// string literals are interned
+
+// String interning is an optimization technique by the compiler.
+// If we have two identical string literals in one compilation unit then
+// the code generated ensures that there is only one string object created
+// for all the instances of that literal.
+
+public class StringIntern {
+
+    public static void main(String[] args) {
+
+        String val1 = "70";
+        String val2 = "70";
+
+        System.out.println(val1 == val2);
+    }
+}
