18edf04fd3d2964dc048a6091c7c9a1769ab79dc
Jan Bodnar <jan.bodnar@gmail.com>
2019-08-03 12:13:20 +0200

    Create RetailAll.java

---
+++ collections/arraylist/RetailAll.java
@@ -0,0 +1,22 @@
+package com.zetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RetailAll {
+
+    public static void main(String[] args) {
+
+        List<String> letters = new ArrayList<>();
+        letters.add("a");
+        letters.add("b");
+        letters.add("c");
+        letters.add("a");
+        letters.add("d");
+
+        System.out.println(letters);
+
+        letters.retainAll(List.of("a", "b"));
+        System.out.println(letters);
+    }
+}
