e399389b37f55317daa90ff87b62e1ccab8fc094
Jan Bodnar <jan.bodnar@gmail.com>
2019-08-04 22:58:51 +0200

    Create SupplierEx2.java

---
+++ functional/SupplierEx2.java
@@ -0,0 +1,20 @@
+package com.zetcode;
+
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+public class SupplierEx2 {
+
+    public static void main(String[] args) {
+
+        Supplier<Stream<String>> streamSupplier =
+                () -> Stream.of("cloud", "wood", "falcon", "sky", "forest")
+                        .filter(s -> s.startsWith("w"));
+
+        var res = streamSupplier.get().anyMatch(s -> true);
+        System.out.println(res);
+
+        var res2 = streamSupplier.get().noneMatch(s -> true);
+        System.out.println(res2);
+    }
+}
