2741ca0144104304ebb5b3265acf5a3864c8362a
Jan Bodnar <jan.bodnar@gmail.com>
2019-08-04 19:20:33 +0200

    Update ConsumerEx.java

--- functional/ConsumerEx.java
+++ functional/ConsumerEx.java
@@ -2,8 +2,8 @@ package com.zetcode;
 
 import java.util.function.Consumer;
 
-// Consumer represents an operation that accepts a single input argument and returns no
-// result. Unlike most other functional interfaces, Consumer is expected
+// Consumer represents an operation that accepts a single input argument and 
+// returns no result. Unlike most other functional interfaces, Consumer is expected
 // to operate via side-effects.
 
 public class ConsumerEx {
