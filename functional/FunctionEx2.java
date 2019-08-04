3401f08414383ac8f8f5db098ce86f2d36427d2f
Jan Bodnar <jan.bodnar@gmail.com>
2019-08-04 23:26:09 +0200

    Create FunctionEx2.java

---
+++ functional/FunctionEx2.java
@@ -0,0 +1,28 @@
+package com.zetcode;
+
+import java.util.ArrayList;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class FunctionEx2 {
+
+    public static void main(String[] args) {
+
+        var values = new ArrayList<Integer>() {{
+            add(1);
+            add(2);
+            add(3);
+            add(4);
+            add(5);
+        }};
+
+        var squared = values.stream().map(new Function<Integer, Object>() {
+            @Override
+            public Object apply(Integer integer) {
+                return integer * integer;
+            }
+        }).collect(Collectors.toList());
+
+        System.out.println(squared);
+    }
+}
