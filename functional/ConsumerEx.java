660f0e437c342a9624ca521ee2806024d90f93e1
Jan Bodnar <jan.bodnar@gmail.com>
2019-08-04 19:10:45 +0200

    Create ConsumerEx.java

---
+++ functional/ConsumerEx.java
@@ -0,0 +1,22 @@
+package com.zetcode;
+
+import java.util.function.Consumer;
+
+// Consumer represents an operation that accepts a single input argument and returns no
+// result. Unlike most other functional interfaces, Consumer is expected
+// to operate via side-effects.
+
+public class ConsumerEx {
+
+    public static void main(String[] args) {
+
+        Consumer<String> showThreeTimes = value -> {
+
+            System.out.println(value);
+            System.out.println(value);
+            System.out.println(value);
+        };
+
+        showThreeTimes.accept("Blue sky");
+    }
+}
