cb0c3b66f6d5fb13e99950b45a5745085b258c50
Jan Bodnar <jan.bodnar@gmail.com>
2019-08-05 13:06:25 +0200

    Create PositiveValues.java

---
+++ validation/PositiveValues.java
@@ -0,0 +1,41 @@
+package com.zetcode;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import javax.validation.constraints.Positive;
+import java.util.List;
+import java.util.Set;
+
+class Data {
+
+    private List<@Positive Integer> values = List.of(2, 3, 4, 5, -4, 3);
+}
+
+public class PositiveValues {
+
+    private static Validator validator;
+
+    public static void main(String[] args) {
+
+        var data = new Data();
+
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+
+        validate(data);
+    }
+
+    private static void validate(Data data) {
+
+        Set<ConstraintViolation<Data>> cvs = validator.validate(data);
+
+        System.out.println(cvs.size());
+
+        for (ConstraintViolation<Data> cv : cvs) {
+
+            System.out.printf("%s: %s%n", cv.getPropertyPath(), cv.getMessage());
+        }
+    }
+}
