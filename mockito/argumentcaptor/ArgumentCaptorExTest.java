847afd48e6c9d2e15eb1156c9f34416b9a4f5c90
Jan Bodnar <jan.bodnar@gmail.com>
2019-08-06 22:16:23 +0200

    Create ArgumentCaptorExTest.java

---
+++ mockito/argumentcaptor/ArgumentCaptorExTest.java
@@ -0,0 +1,41 @@
+package com.zetcode;
+
+import com.zetcode.service.MessageService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+@RunWith(JUnitPlatform.class)
+class ArgumentCaptorExTest {
+
+    @Mock
+    private MessageService messageService;
+
+    @Captor
+    private ArgumentCaptor<String> captor1;
+
+    @Captor
+    private ArgumentCaptor<LocalDateTime> captor2;
+
+    @Test
+    public void testGetMessage() {
+
+        messageService.getMessage("Hello!", LocalDateTime.of(2019, 8, 20, 20, 18));
+
+        Mockito.verify(messageService).getMessage(captor1.capture(), captor2.capture());
+
+        assertEquals("Hello!", captor1.getValue());
+        assertEquals(LocalDateTime.of(2019, 8, 20, 20, 18), captor2.getValue());
+    }
+}
