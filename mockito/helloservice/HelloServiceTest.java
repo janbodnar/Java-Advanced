1d474832cd61ecc21c2ff029d94b8228a93c8fc5
Jan Bodnar <jan.bodnar@gmail.com>
2019-08-06 21:42:10 +0200

    Create HelloServiceTest.java

---
+++ mockito/helloservice/HelloServiceTest.java
@@ -0,0 +1,26 @@
+package com.zetcode.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+@RunWith(JUnitPlatform.class)
+class HelloServiceTest {
+
+    @Mock
+    private HelloService helloService;
+
+    @Test
+    public void testHelloMessage() {
+
+        when(helloService.getMessage()).thenReturn("Hello!");
+        assertEquals(helloService.getMessage(), "Hello!");
+    }
+}
