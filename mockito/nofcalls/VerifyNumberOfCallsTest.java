423dd1a33710b33c365391cd62fd74ca0ee43718
Jan Bodnar <jan.bodnar@gmail.com>
2019-08-07 13:00:56 +0200

    Create VerifyNumberOfCallsTest.java

---
+++ mockito/nofcalls/VerifyNumberOfCallsTest.java
@@ -0,0 +1,28 @@
+package com.zetcode;
+
+import com.zetcode.service.MessageService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+@RunWith(JUnitPlatform.class)
+class VerifyNumberOfCallsTest {
+
+    @Mock
+    private MessageService messageService;
+
+    @Test
+    void testSayMessage() {
+
+        messageService.say();
+        messageService.say();
+        messageService.say();
+
+        Mockito.verify(messageService, Mockito.times(3)).say();
+    }
+}
