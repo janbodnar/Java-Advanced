c2bf1814b18338e58a72248d348fe3e3ae250747
Jan Bodnar <jan.bodnar@gmail.com>
2019-08-07 14:02:34 +0200

    Update ArgumentCaptorExTest.java

--- mockito/argumentcaptor/ArgumentCaptorExTest.java
+++ mockito/argumentcaptor/ArgumentCaptorExTest.java
@@ -17,7 +17,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 @RunWith(JUnitPlatform.class)
-class ArgumentCaptorExTest {
+class MessageServiceTest {
 
     @Mock
     private MessageService messageService;
