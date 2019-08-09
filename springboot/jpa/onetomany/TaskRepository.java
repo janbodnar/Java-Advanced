8643fdd72e9292f021aad7f604f82202cca26ab5
Jan Bodnar <jan.bodnar@gmail.com>
2019-08-09 19:31:08 +0200

    Create TaskRepository.java

---
+++ springboot/jpa/onetomany/TaskRepository.java
@@ -0,0 +1,10 @@
+package com.zetcode.repository;
+
+import com.zetcode.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+
+}
