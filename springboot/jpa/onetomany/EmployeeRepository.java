54163d951ba992e7145885c7c348001dbc219edf
Jan Bodnar <jan.bodnar@gmail.com>
2019-08-09 19:30:56 +0200

    Create EmployeeRepository.java

---
+++ springboot/jpa/onetomany/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.zetcode.repository;
+
+import com.zetcode.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
