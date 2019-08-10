8fa0a0c206a2add39cce7eb90a7571a77a3914cc
Jan Bodnar <jan.bodnar@gmail.com>
2019-08-10 20:09:45 +0200

    Create Message.java

---
+++ cdi/producer/Message.java
@@ -0,0 +1,19 @@
+package com.zetcode.producer;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import javax.inject.Qualifier;
+
+@Qualifier
+@Retention(RUNTIME)
+@Target({FIELD, TYPE, METHOD})
+public @interface Message {
+
+    MessageType value();
+}
