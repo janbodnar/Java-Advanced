031f6506239cb1d12609776383f941b3ef2e758b
Jan Bodnar <jan.bodnar@gmail.com>
2019-08-10 20:09:29 +0200

    Create MessageFactory.java

---
+++ cdi/producer/MessageFactory.java
@@ -0,0 +1,56 @@
+package com.zetcode.factory;
+
+import com.zetcode.producer.Message;
+import com.zetcode.producer.MessageType;
+import com.zetcode.provider.HelloMessageProvider;
+import com.zetcode.provider.TimeMessageProvider;
+
+import javax.enterprise.inject.Produces;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class MessageFactory {
+
+    private Properties properties;
+
+    @Produces
+    @Message(MessageType.HELLO_MESSAGE)
+    public HelloMessageProvider getHelloMessageProvider() {
+        return new HelloMessageProvider();
+    }
+
+    @Produces
+    @Message(MessageType.TIME_MESSAGE)
+    public TimeMessageProvider getTimeMessageProvider() {
+
+        loadProperties();
+
+        var format = String.valueOf(properties.get("timeFormat"));
+
+        var timeMessageProvider = new TimeMessageProvider();
+        timeMessageProvider.setFormat(format);
+        return timeMessageProvider;
+    }
+
+    private void loadProperties() {
+
+        this.properties = new Properties();
+        final InputStream stream = MessageFactory.class
+                .getResourceAsStream("/application.properties");
+
+        if (stream == null) {
+
+            throw new RuntimeException("properties file not found");
+        }
+
+        try {
+
+            this.properties.load(stream);
+
+        } catch (IOException e) {
+
+            throw new RuntimeException("failed to load configuration");
+        }
+    }
+}
