fb79107ff0e4c42a4a458373317fc40cf345ad0e
Jan Bodnar <jan.bodnar@gmail.com>
2019-08-12 11:07:25 +0200

    Create EventEx.java

---
+++ cdi/event/EventEx.java
@@ -0,0 +1,24 @@
+package com.zetcode;
+
+import com.zetcode.service.TextService;
+import com.zetcode.board.NotifyBoard;
+
+import javax.enterprise.inject.se.SeContainerInitializer;
+
+public class EventEx {
+
+    public static void main(String[] args) {
+
+        var initializer = SeContainerInitializer.newInstance();
+
+        try (var container = initializer.disableDiscovery()
+                .addBeanClasses(TextService.class, NotifyBoard.class).initialize()) {
+
+            var value = "old falcon";
+
+            var textService = container.select(TextService.class).get();
+            var modified = textService.upper(value);
+            System.out.println(modified);
+        }
+    }
+}
