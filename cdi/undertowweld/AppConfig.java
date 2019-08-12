b22094d66cbf1a6332b9e7a52438dca379ba595b
Jan Bodnar <jan.bodnar@gmail.com>
2019-08-12 13:27:45 +0200

    Create AppConfig.java

---
+++ cdi/undertowweld/AppConfig.java
@@ -0,0 +1,19 @@
+package com.zetcode.conf;
+
+import com.zetcode.ws.MessageResource;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
+
+@ApplicationPath("/")
+public class AppConfig extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> set = new HashSet<>();
+        set.add(MessageResource.class);
+        return set;
+    }
+}
