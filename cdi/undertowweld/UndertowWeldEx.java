110cf5582a72a6a01f0f4c62a7837da016419784
Jan Bodnar <jan.bodnar@gmail.com>
2019-08-12 13:28:28 +0200

    Create UndertowWeldEx.java

---
+++ cdi/undertowweld/UndertowWeldEx.java
@@ -0,0 +1,35 @@
+package com.zetcode;
+
+import com.zetcode.conf.AppConfig;
+import io.undertow.Undertow;
+import io.undertow.servlet.Servlets;
+import io.undertow.servlet.api.DeploymentInfo;
+import org.jboss.resteasy.core.ResteasyDeploymentImpl;
+import org.jboss.resteasy.plugins.server.undertow.UndertowJaxrsServer;
+import org.jboss.resteasy.spi.ResteasyDeployment;
+import org.jboss.weld.environment.servlet.Listener;
+
+// ttp://localhost:8080/api/sayhello
+
+public class UndertowWeldEx {
+
+    public static void main(String[] args) {
+
+        var server = new UndertowJaxrsServer();
+        Undertow.Builder serverBuilder = Undertow.builder().addHttpListener(8080, "0.0.0.0");
+
+        ResteasyDeployment deployment = new ResteasyDeploymentImpl();
+
+        DeploymentInfo deploymentInfo = server.undertowDeployment(deployment, "/");
+        deployment.setApplicationClass(AppConfig.class.getName());
+        deployment.setInjectorFactoryClass("org.jboss.resteasy.cdi.CdiInjectorFactory");
+
+        deploymentInfo.setClassLoader(UndertowWeldEx.class.getClassLoader());
+        deploymentInfo.setDeploymentName("My Application");
+        deploymentInfo.setContextPath("/api");
+        deploymentInfo.addListeners(Servlets.listener(Listener.class));
+
+        server.deploy(deploymentInfo);
+        server.start(serverBuilder);
+    }
+}
