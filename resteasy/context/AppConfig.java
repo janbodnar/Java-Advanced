3f9b33d106c615e6895f48a15f4d60b440b600f5
Jan Bodnar <jan.bodnar@gmail.com>
2019-08-12 12:42:57 +0200

    Create AppConfig.java

---
+++ resteasy/context/AppConfig.java
@@ -0,0 +1,18 @@
+package com.zetcode.conf;
+
+import com.zetcode.ws.InfoResource;
+import java.util.HashSet;
+import java.util.Set;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class AppConfig extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> set = new HashSet<>();
+        set.add(InfoResource.class);
+        return set;
+    }
+}
