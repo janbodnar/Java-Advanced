d2c753ee3c99cd8fb6fa1acc5c3d79f654e6373b
Jan Bodnar <jan.bodnar@gmail.com>
2019-08-12 14:04:34 +0200

    Create City.java

---
+++ resteasy/jsonres/City.java
@@ -0,0 +1,86 @@
+package com.zetcode.model;
+
+import java.util.Objects;
+
+public class City {
+
+    private Long id;
+    private String name;
+    private int population;
+
+    public City() {
+    }
+
+    public City(Long id, String name, int population) {
+        this.id = id;
+        this.name = name;
+        this.population = population;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 71 * hash + Objects.hashCode(this.id);
+        hash = 71 * hash + Objects.hashCode(this.name);
+        hash = 71 * hash + this.population;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final City other = (City) obj;
+        if (this.population != other.population) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        return Objects.equals(this.id, other.id);
+    }
+
+    @Override
+    public String toString() {
+
+        final StringBuilder sb = new StringBuilder("City{");
+
+        sb.append("id=").append(id);
+        sb.append(", name='").append(name).append('\'');
+        sb.append(", population=").append(population);
+        sb.append('}');
+
+        return sb.toString();
+    }
+}
