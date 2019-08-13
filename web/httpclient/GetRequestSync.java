86235459d6715d4fdf7e86398abeee3e489fca44
Jan Bodnar <jan.bodnar@gmail.com>
2019-08-13 19:28:54 +0200

    Create GetRequestSync.java

---
+++ web/httpclient/GetRequestSync.java
@@ -0,0 +1,23 @@
+package com.zetcode;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class GetRequestSync {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://webcode.me"))
+                .build();
+
+        HttpResponse<String> response = client.send(request,
+                HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
+    }
+}
