5ae9a9b99d4a153ef01e394be5bffa4b82ed6ded
Jan Bodnar <jan.bodnar@gmail.com>
2019-08-16 16:39:32 +0200

    Create RunTasksCompletableFuture.java

---
+++ async/RunTasksCompletableFuture.java
@@ -0,0 +1,58 @@
+package com.zetcode;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.toList;
+
+class Task {
+
+    private final int duration;
+
+    public Task(int duration) {
+        this.duration = duration;
+    }
+
+    public int doTask() {
+        System.out.printf("%s %n", Thread.currentThread().getName());
+
+        try {
+            Thread.sleep(duration * 1000);
+        } catch (final InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        return duration;
+    }
+}
+
+public class RunTasksCompletableFuture {
+
+    public static void main(String[] args) {
+
+        List<Task> tasks = IntStream.range(0, 10)
+                .mapToObj(i -> new Task(1))
+                .collect(toList());
+
+        List<CompletableFuture<Integer>> futures =
+                tasks.stream()
+                        .map(task -> CompletableFuture.supplyAsync(task::doTask))
+                        .collect(Collectors.toList());
+
+        long start = System.nanoTime();
+
+        List<Integer> result =
+                futures.stream()
+                        .map(CompletableFuture::join)
+                        .collect(Collectors.toList());
+
+        long end = System.nanoTime();
+
+        long duration = (end - start) / 1_000_000;
+
+        System.out.printf("Run %d tasks in %d millis\n", tasks.size(), duration);
+        System.out.println(result);
+    }
+}
