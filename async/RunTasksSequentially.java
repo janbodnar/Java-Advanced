f5f163c3c357d36adc14e9b2e2d1fe574c2af53c
Jan Bodnar <jan.bodnar@gmail.com>
2019-08-16 16:33:21 +0200

    Create RunTasksSequentially.java

---
+++ async/RunTasksSequentially.java
@@ -0,0 +1,51 @@
+package com.zetcode;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.toList;
+
+class Task {
+
+    private final int duration;
+
+    public Task(int duration) {
+        this.duration = duration;
+    }
+
+    public int doTask() {
+        System.out.printf("%s ", Thread.currentThread().getName());
+
+        try {
+            Thread.sleep(duration * 1000);
+        } catch (final InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        return duration;
+    }
+}
+
+public class RunTasksSequentially {
+
+    public static void main(String[] args) {
+
+        List<Task> tasks = IntStream.range(0, 10)
+                .mapToObj(i -> new Task(1))
+                .collect(toList());
+
+        long start = System.nanoTime();
+
+        List<Integer> result = tasks.stream()
+                .map(Task::doTask)
+                .collect(toList());
+
+        long end = System.nanoTime();
+
+        long duration = (end - start) / 1_000_000;
+
+        System.out.printf("%nRun %d tasks in %d millis\n", tasks.size(), duration);
+        System.out.println(result);
+
+    }
+}
