9f9cb68dee8f79d75c8e6dbb273f300215d06636
Jan Bodnar <jan.bodnar@gmail.com>
2019-08-16 14:03:10 +0200

    Create Task.java

---
+++ web/httpclient/post-json/Task.java
@@ -0,0 +1,41 @@
+package com.zetcode.bean;
+
+public class Task {
+
+    private Long id;
+    private String name;
+
+    public Task() {}
+
+    public Task(Long id, String name) {
+
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+
+        final StringBuilder sb = new StringBuilder("Task{");
+        sb.append("id=").append(id);
+        sb.append(", name='").append(name).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
