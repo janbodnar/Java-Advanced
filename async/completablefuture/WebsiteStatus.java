2ae4f4ad1c9b1ba0ad4cac9f78539e4618be96d4
Jan Bodnar <jan.bodnar@gmail.com>
2019-08-16 15:18:55 +0200

    Create WebsiteStatus.java

---
+++ async/completablefuture/WebsiteStatus.java
@@ -0,0 +1,55 @@
+package com.zetcode;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+public class WebsiteStatus {
+
+    public static void main(String[] args) {
+
+        List<URI> uris = Stream.of(
+                "https://www.google.com/",
+                "https://www.sme.sk/",
+                "https://www.github.com/"
+        ).map(URI::create).collect(toList());
+
+        HttpClient httpClient = HttpClient.newBuilder()
+                .connectTimeout(Duration.ofSeconds(10))
+                .followRedirects(HttpClient.Redirect.ALWAYS)
+                .build();
+
+        CompletableFuture[] futures = uris.stream()
+                .map(uri -> verifyUri(httpClient, uri))
+                .toArray(CompletableFuture[]::new);
+
+        CompletableFuture.allOf(futures).join();
+    }
+
+    private static CompletableFuture verifyUri(HttpClient httpClient,
+                                                     URI uri) {
+        HttpRequest request = HttpRequest.newBuilder()
+                .timeout(Duration.ofSeconds(5))
+                .uri(uri)
+                .build();
+
+        return httpClient.sendAsync(request, HttpResponse.BodyHandlers.discarding())
+                .thenApply(HttpResponse::statusCode)
+                .thenApply(statusCode -> statusCode == 200)
+                .exceptionally(ex -> false)
+                .thenAccept(valid -> {
+                    if (valid) {
+                        System.out.printf("[SUCCESS] Verified %s%n", uri);
+                    } else {
+                        System.out.printf("[FAILURE] Failed to verify%s%n", uri);
+                    }
+                });
+    }
+}
