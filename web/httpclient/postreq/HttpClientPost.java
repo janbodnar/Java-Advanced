26f9038eebabf76d76793424bcab60ac37adc4ee
Jan Bodnar <jan.bodnar@gmail.com>
2019-08-16 13:14:59 +0200

    Create HttpClientPost.java

---
+++ web/httpclient/postreq/HttpClientPost.java
@@ -0,0 +1,36 @@
+package com.zetcode;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.HashMap;
+
+public class HttpClientPost {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        var values = new HashMap<String, String>() {{
+            put("name", "John Doe");
+            put ("occupation", "gardener");
+        }};
+
+        var objectMapper = new ObjectMapper();
+        String requestBody = objectMapper
+                .writeValueAsString(values);
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://httpbin.org/post"))
+                .POST(HttpRequest.BodyPublishers.ofString(requestBody))
+                .build();
+
+        HttpResponse<String> response = client.send(request,
+                HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
+    }
+}
