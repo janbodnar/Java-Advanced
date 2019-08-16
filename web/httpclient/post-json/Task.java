6fc3fb6c416e4d413a489061f9b387f29fd84d7a
Jan Bodnar <jan.bodnar@gmail.com>
2019-08-16 14:13:32 +0200

    Update Task.java

--- web/httpclient/post-json/Task.java
+++ web/httpclient/post-json/Task.java
@@ -1,5 +1,7 @@
 package com.zetcode.bean;
 
+import java.util.Objects;
+
 public class Task {
 
     private Long id;
@@ -27,6 +29,20 @@ public class Task {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return Objects.equals(id, task.id) &&
+                Objects.equals(name, task.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
     }
 
     @Override
