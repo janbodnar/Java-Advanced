e0ab2454184c57b5da904c832e6b70563fa54d21
Jan Bodnar <jan.bodnar@gmail.com>
2019-08-16 18:55:50 +0200

    Create GetBlocking.java

---
+++ async/completablefuture/GetBlocking.java
@@ -0,0 +1,29 @@
+package com.zetcode;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+public class GetBlocking {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+
+        CompletableFuture<Void> future = CompletableFuture.runAsync(() -> {
+
+            // Simulate a long-running job
+            try {
+                TimeUnit.SECONDS.sleep(3);
+            } catch (InterruptedException e) {
+                throw new IllegalStateException(e);
+            }
+
+            System.out.printf("Task run in: %s %n", Thread.currentThread().getName());
+        });
+
+        future.whenComplete((aVoid, throwable) -> System.out.println("completed"));
+
+        future.get();
+
+        System.out.println("Program finished");
+    }
+}
