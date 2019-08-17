3de010ee3566756874d9d4aa4a03afe832354ac2
Jan Bodnar <jan.bodnar@gmail.com>
2019-08-17 13:05:19 +0200

    Create MyHandler.java

---
+++ web/embeddedjetty/plaintext/MyHandler.java
@@ -0,0 +1,21 @@
+package com.zetcode;
+
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MyHandler extends AbstractHandler {
+
+    @Override
+    public void handle(String target, Request baseRequest, HttpServletRequest request,
+                       HttpServletResponse response) throws IOException {
+
+        response.setContentType("text/plain;charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+        baseRequest.setHandled(true);
+        response.getWriter().println("Hello there");
+    }
+}
