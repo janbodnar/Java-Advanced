163298946014626779bd18b63bd1620e45ecf3d7
Jan Bodnar <jan.bodnar@gmail.com>
2019-08-17 12:08:20 +0200

    Create MyServlet.java

---
+++ web/servlet/forwardservlet/MyServlet.java
@@ -0,0 +1,24 @@
+package com.zetcode.web;
+
+import java.io.IOException;
+import java.time.Instant;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "MyServlet", urlPatterns = {"/MyServlet"})
+public class MyServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html;charset=UTF-8");
+
+        request.setAttribute("now", Instant.now());
+
+        request.getRequestDispatcher("AnotherServlet").forward(request, response);
+    }
+}
