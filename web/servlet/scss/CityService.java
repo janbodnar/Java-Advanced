e0d9330dd0ef1e3184253d57a078ae296b074282
Jan Bodnar <jan.bodnar@gmail.com>
2019-08-17 11:58:23 +0200

    Create CityService.java

---
+++ web/servlet/scss/CityService.java
@@ -0,0 +1,24 @@
+package com.zetcode.service;
+
+import com.zetcode.bean.City;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CityService implements ICityService {
+
+    public List<City> findAll() {
+
+        List<City> cities = new ArrayList<>();
+
+        cities.add(new City(1L, "Bratislava", 432000));
+        cities.add(new City(2L, "Budapest", 1759000));
+        cities.add(new City(3L, "Prague", 1280000));
+        cities.add(new City(4L, "Warsaw", 1748000));
+        cities.add(new City(5L, "Los Angeles", 3971000));
+        cities.add(new City(6L, "New York", 8550000));
+        cities.add(new City(7L, "Edinburgh", 464000));
+        cities.add(new City(8L, "Berlin", 3671000));
+
+        return cities;
+    }
+}
