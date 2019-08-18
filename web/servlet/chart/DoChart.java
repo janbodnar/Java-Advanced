af25465571f29f15262e873bfddf7c12bfb2f778
Jan Bodnar <jan.bodnar@gmail.com>
2019-08-18 17:03:51 +0200

    Update DoChart.java

--- web/servlet/chart/DoChart.java
+++ web/servlet/chart/DoChart.java
@@ -3,6 +3,8 @@ package com.zetcode.web;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartUtils;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
 import org.jfree.data.general.DefaultPieDataset;
 
 import javax.servlet.annotation.WebServlet;
@@ -30,21 +32,42 @@ public class DoChart extends HttpServlet {
         ChartUtils.writeChartAsPNG(os, chart, width, height);
     }
 
+//    public JFreeChart getChart() {
+//
+//        var dataset = new DefaultPieDataset();
+//
+//        dataset.setValue("Croatia", 22);
+//        dataset.setValue("Bohemia", 34);
+//        dataset.setValue("Bulgaria", 18);
+//        dataset.setValue("Spain", 5);
+//        dataset.setValue("Others", 21);
+//
+//        JFreeChart chart = ChartFactory.createPieChart("Popular destinations",
+//                dataset, true, false, false);
+//
+//        chart.setBorderVisible(false);
+//
+//        return chart;
+//    }
+
     public JFreeChart getChart() {
 
-        var dataset = new DefaultPieDataset();
+        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+        dataset.setValue(46, "Gold medals", "USA");
+        dataset.setValue(38, "Gold medals", "China");
+        dataset.setValue(29, "Gold medals", "UK");
+        dataset.setValue(22, "Gold medals", "Russia");
+        dataset.setValue(13, "Gold medals", "South Korea");
+        dataset.setValue(11, "Gold medals", "Germany");
 
-        dataset.setValue("Croatia", 22);
-        dataset.setValue("Bohemia", 34);
-        dataset.setValue("Bulgaria", 18);
-        dataset.setValue("Spain", 5);
-        dataset.setValue("Others", 21);
+        JFreeChart barChart = ChartFactory.createBarChart(
+                "Olympic gold medals in London",
+                "",
+                "Gold medals",
+                dataset,
+                PlotOrientation.VERTICAL,
+                false, true, false);
 
-        JFreeChart chart = ChartFactory.createPieChart("Popular destinations",
-                dataset, true, false, false);
-
-        chart.setBorderVisible(false);
-
-        return chart;
+        return barChart;
     }
 }
