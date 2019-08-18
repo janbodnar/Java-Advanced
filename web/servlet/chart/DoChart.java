7c993265a3dc9cfb77717bd585d04b88cb973865
Jan Bodnar <jan.bodnar@gmail.com>
2019-08-18 16:40:09 +0200

    Create DoChart.java

---
+++ web/servlet/chart/DoChart.java
@@ -0,0 +1,50 @@
+package com.zetcode.web;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartUtils;
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.general.DefaultPieDataset;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+
+@WebServlet(name = "DoChart", urlPatterns = {"/showChart"})
+public class DoChart extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+
+        response.setContentType("image/png");
+
+        OutputStream os = response.getOutputStream();
+
+        JFreeChart chart = getChart();
+        int width = 500;
+        int height = 350;
+
+        ChartUtils.writeChartAsPNG(os, chart, width, height);
+    }
+
+    public JFreeChart getChart() {
+
+        var dataset = new DefaultPieDataset();
+
+        dataset.setValue("Croatia", 22);
+        dataset.setValue("Bohemia", 34);
+        dataset.setValue("Bulgaria", 18);
+        dataset.setValue("Spain", 5);
+        dataset.setValue("Others", 21);
+
+        JFreeChart chart = ChartFactory.createPieChart("Popular destinations",
+                dataset, true, false, false);
+
+        chart.setBorderVisible(false);
+
+        return chart;
+    }
+}
