082a62d09d3b8ef9d51dfc2a0d0f15857f2323ab
Jan Bodnar <jan.bodnar@gmail.com>
2019-09-02 16:12:42 +0200

    Create CombineCompletedWithAsyncFuture.java

---
+++ async/completablefuture/CombineCompletedWithAsyncFuture.java
@@ -0,0 +1,38 @@
+package com.zetcode;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.function.BinaryOperator;
+import java.util.function.Supplier;
+
+class NewWord implements Supplier<String> {
+
+    @Override
+    public String get() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return "deep sky";
+    }
+}
+
+// we combine an completed future with an async task
+// the two results are passed as arguments to the supplied function (transform)
+
+public class CombineCompletedWithAsyncFuture {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+
+        CompletableFuture<String> startValue = CompletableFuture.completedFuture("deep ocean");
+        CompletableFuture<String> future = CompletableFuture.supplyAsync(new NewWord());
+
+        BinaryOperator<String> transform = (f, s) -> f.toUpperCase() + "  " + s.toUpperCase();
+        CompletableFuture<String> combinedFuture = future.thenCombine(startValue, transform);
+
+        var result = combinedFuture.get();
+        System.out.println(result);
+    }
+}
