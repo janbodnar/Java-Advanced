82f623d7b61899c9a748332d72841263432cc2e3
Jan Bodnar <jan.bodnar@gmail.com>
2020-01-12 13:11:05 +0100

    Create ConcatStrings2.java

---
+++ strings/ConcatStrings2.java
@@ -0,0 +1,20 @@
+package com.zetcode;
+
+public class ConcatStrings2 {
+
+    public static void main(String[] args) {
+
+        int age = 34;
+        String name = "Peter";
+
+        System.out.println(name + " is " + age + " years old");
+        System.out.printf("%s is %d years old%n", name, age);
+        System.out.println(String.format("%s is %d years old", name, age));
+
+        var sb = new StringBuilder(name);
+        sb.append(" is ").append(age).append(" years old");
+        System.out.println(sb);
+
+        System.out.println(name.concat(" is ").concat(String.valueOf(age)).concat(" years old"));
+    }
+}
