a9a96b656006fdc4e3a9232761adf3a5d35ca5fb
Jan Bodnar <jan.bodnar@gmail.com>
2020-01-18 16:02:58 +0100

    Create CapitalizeWords.java

---
+++ CapitalizeWords.java
@@ -0,0 +1,27 @@
+package com.zetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class CapitalizeWords {
+
+    public static void main(String[] args) {
+
+        List<String> words = List.of("rock", "forest", "sky", "cloud", "water");
+        List<String> capitalized = new ArrayList<>();
+
+        for (var word : words) {
+
+            var sb = new StringBuilder(word);
+
+            char c = sb.charAt(0);
+            char upperCased = Character.toUpperCase(c);
+            sb.setCharAt(0, upperCased);
+
+            capitalized.add(sb.toString());
+        }
+
+        System.out.println(capitalized);
+    }
+}
