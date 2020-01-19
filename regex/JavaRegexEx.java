1c21ac4a403ba1f8cf1e562c3c8f5043e19a111a
Jan Bodnar <jan.bodnar@gmail.com>
2020-01-19 23:11:53 +0100

    Update JavaRegexEx.java

--- regex/JavaRegexEx.java
+++ regex/JavaRegexEx.java
@@ -1,31 +1,32 @@
 package com.zetcode;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 // Regular expression - defines a search pattern for strings
 // Pattern - compiled representation of a regular expression
-// Matcher - engine that interprets the pattern and performs 
+// Matcher - engine that interprets the pattern and performs
 //           match operations against an input string. 
 
 public class JavaRegexEx {
 
     public static void main(String[] args) {
 
-        List<String> words = Arrays.asList("Seven", "even",
+        List<String> words = List.of("Seven", "even",
                 "Maven", "Amen", "eleven");
 
         Pattern p = Pattern.compile(".even");
-        
+
         for (String word: words) {
-            
-            Matcher m = p.matcher(word);  
-            
+
+            Matcher m = p.matcher(word);
+
             if (m.matches()) {
+
                 System.out.printf("%s matches%n", word);
             } else {
+
                 System.out.printf("%s does not match%n", word);
             }
         }
