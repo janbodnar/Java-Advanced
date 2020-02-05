6bcee46284ee39f6563fc613ebd70dfb94bc4b83
Jan Bodnar <jan.bodnar@gmail.com>
2020-02-06 00:42:52 +0100

    Create TestTitle.java

---
+++ junit5/selenium/TestTitle.java
@@ -0,0 +1,40 @@
+package com.zetcode.web;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+class TestTitle {
+
+    private static WebDriver driver;
+
+    @BeforeAll
+    static void setUp() {
+
+        var opt = new FirefoxOptions();
+        opt.setHeadless(true);
+
+        driver = new FirefoxDriver(opt);
+    }
+
+    @Test
+    public void test() {
+
+        driver.get("http://webcode.me");
+
+        System.out.println("Page title is: " + driver.getTitle());
+        Assertions.assertEquals("My html page", driver.getTitle());
+    }
+
+    @AfterAll
+    static void tearDown() {
+
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
