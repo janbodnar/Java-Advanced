d9a89725e00cf7dcb5600b90d5d918a2ac7cae39
Jan Bodnar <jan.bodnar@gmail.com>
2020-06-26 16:55:36 +0200

    Update OpenCSVWriteBeansEx.java

--- csv/writebeans/OpenCSVWriteBeansEx.java
+++ csv/writebeans/OpenCSVWriteBeansEx.java
@@ -6,13 +6,12 @@ import com.opencsv.bean.StatefulBeanToCsvBuilder;
 import com.opencsv.exceptions.CsvDataTypeMismatchException;
 import com.opencsv.exceptions.CsvRequiredFieldEmptyException;
 import com.zetcode.bean.Car;
-import java.io.BufferedWriter;
+
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -24,18 +23,18 @@ public class OpenCSVWriteBeansEx {
         String fileName = "src/main/resources/cars.csv";
         Path myPath = Paths.get(fileName);
 
-        List<Car> cars = new ArrayList<>();
-        cars.add(new Car(1, "Audi", 52642));
-        cars.add(new Car(2, "Mercedes", 57127));
-        cars.add(new Car(3, "Skoda", 9000));
-        cars.add(new Car(4, "Volvo", 29000));
+        var cars = List.of(new Car(1, "Audi", 52642),
+                new Car(2, "Mercedes", 57127),
+                new Car(3, "Skoda", 9000),
+                new Car(4, "Volvo", 29000));
 
-        try (BufferedWriter writer = Files.newBufferedWriter(myPath,
-                StandardCharsets.UTF_8)) {
 
-            StatefulBeanToCsv<Car> beanToCsv = new StatefulBeanToCsvBuilder(writer)
+        try (var writer = Files.newBufferedWriter(myPath, StandardCharsets.UTF_8)) {
+
+            StatefulBeanToCsv<Car> beanToCsv = new StatefulBeanToCsvBuilder<Car>(writer)
                     .withQuotechar(CSVWriter.NO_QUOTE_CHARACTER)
                     .build();
+
             beanToCsv.write(cars);
 
         } catch (CsvDataTypeMismatchException | CsvRequiredFieldEmptyException |
