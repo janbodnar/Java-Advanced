7fe1246a76af33ae2fac8dcd54f6dd62fe9e6b49
Jan Bodnar <jan.bodnar@gmail.com>
2020-06-26 15:40:05 +0200

    Update OpenCSVDatabaseEx.java

--- csv/mysql/OpenCSVDatabaseEx.java
+++ csv/mysql/OpenCSVDatabaseEx.java
@@ -1,16 +1,13 @@
 package com.zetcode;
 
 import com.opencsv.CSVWriter;
-import java.io.FileWriter;
+
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -19,18 +16,18 @@ public class OpenCSVDatabaseEx {
 
     public static void main(String[] args) {
 
-        String url = "jdbc:mysql://localhost:3306/testdb?useSsl=false";
-        String user = "testuser";
-        String password = "test623";
+        String url = "jdbc:mysql://localhost:3306/mydb?serverTimezone=UTC&useSsl=false";
+        String user = "root";
+        String password = "andrea";
 
         String fileName = "src/main/resources/cars.csv";
         Path myPath = Paths.get(fileName);
 
-        try (Connection con = DriverManager.getConnection(url, user, password);
-                PreparedStatement pst = con.prepareStatement("SELECT * FROM Cars");
-                ResultSet rs = pst.executeQuery()) {
+        try (var con = DriverManager.getConnection(url, user, password);
+             var pst = con.prepareStatement("SELECT * FROM cars");
+             var rs = pst.executeQuery()) {
 
-            try (CSVWriter writer = new CSVWriter(Files.newBufferedWriter(myPath,
+            try (var writer = new CSVWriter(Files.newBufferedWriter(myPath,
                     StandardCharsets.UTF_8), CSVWriter.DEFAULT_SEPARATOR,
                     CSVWriter.NO_QUOTE_CHARACTER, CSVWriter.NO_ESCAPE_CHARACTER,
                     CSVWriter.DEFAULT_LINE_END)) {
