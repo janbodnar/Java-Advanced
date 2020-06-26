c4388de7d9dcfc8b6f2e12d5596a98142d627a3b
Jan Bodnar <jan.bodnar@gmail.com>
2020-06-26 15:19:45 +0200

    Update OpenCSVWriteEx.java

--- csv/OpenCSVWriteEx.java
+++ csv/OpenCSVWriteEx.java
@@ -10,15 +10,14 @@ public class OpenCSVWriteEx {
 
     public static void main(String[] args) throws IOException {
 
-        String[] entries = { "book", "coin", "pencil", "cup" }; 
+        String[] entries = { "book", "coin", "pencil", "cup" };
         String fileName = "src/main/resources/items.csv";
-        
-        try (FileOutputStream fos = new FileOutputStream(fileName);
-                OutputStreamWriter osw = new OutputStreamWriter(fos, 
-                        StandardCharsets.UTF_8);
-                CSVWriter writer = new CSVWriter(osw)) {
-            
+
+        try (var fos = new FileOutputStream(fileName); 
+             var osw = new OutputStreamWriter(fos, StandardCharsets.UTF_8);
+             var writer = new CSVWriter(osw)) {
+
             writer.writeNext(entries);
-        }        
+        }
     }
 }
