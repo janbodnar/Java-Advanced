0380a795356aa36f4671d6bb6cb599a9df16ad79
Jan Bodnar <jan.bodnar@gmail.com>
2020-07-01 13:10:25 +0200

    Create ConsumerForEachEx.java

---
+++ generics/ConsumerForEachEx.java
@@ -0,0 +1,27 @@
+package com.zetcode;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ConsumerForEachEx {
+
+    public static void main(String[] args) {
+
+        var data = List.of(1, 2, 3, 4, 5, 6, 7);
+
+//      Consumer<Integer> consumer = (Integer x) ->  System.out.println(x);
+        Consumer<Integer> consumer = System.out::println;
+        forEach(data, consumer);
+
+        System.out.println("--------------------------");
+
+        forEach(data, System.out::println);
+    }
+
+    static <T> void forEach(List<T> data, Consumer<T> consumer) {
+
+        for (T t : data) {
+            consumer.accept(t);
+        }
+    }
+}
