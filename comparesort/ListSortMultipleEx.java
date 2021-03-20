3cd7dc4dcc05bc7402eb92824ce7fd7873225f13
Jan Bodnar <jan.bodnar@gmail.com>
2021-03-20 22:44:29 +0100

    Update ListSortMultipleEx.java

--- comparesort/ListSortMultipleEx.java
+++ comparesort/ListSortMultipleEx.java
@@ -3,57 +3,9 @@ package com.zetcode;
 import java.util.Arrays;
 import java.util.Comparator;
 
-// Comparing list of objects by multiple object fields
+// Sorting list of objects by multiple object fields
 
-class Person {
-
-    private String name;
-    private int age;
-    private String city;
-
-    public Person(String name, int age, String city) {
-        this.name = name;
-        this.age = age;
-        this.city = city;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    @Override
-    public String toString() {
-
-        final var sb = new StringBuilder("Person{");
-        sb.append("name='").append(name).append('\'');
-        sb.append(", age=").append(age);
-        sb.append(", city='").append(city).append('\'');
-        sb.append('}');
-        return sb.toString();
-    }
-}
-
-public class ListSortMultipleEx {
+public class ListSortMultiple {
 
     public static void main(String[] args) {
 
@@ -71,10 +23,12 @@ public class ListSortMultipleEx {
                 new Person("Sarah", 41, "New York")
         );
 
-        persons.sort(Comparator.comparing(Person::getName)
-                .thenComparing(Person::getCity)
-                .thenComparing(Person::getAge));
+        persons.sort(Comparator.comparing(Person::name)
+                .thenComparing(Person::city)
+                .thenComparing(Person::age));
 
         persons.forEach(System.out::println);
     }
 }
+
+record Person(String name, int age, String city) {}
