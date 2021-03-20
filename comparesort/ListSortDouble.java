c9220b40a81ac1659a2b2a2f46a38c8d76ff04fd
Jan Bodnar <jan.bodnar@gmail.com>
2021-03-20 22:35:29 +0100

    Update and rename ListSortDoubleEx.java to ListSortDouble.java

---
+++ comparesort/ListSortDouble.java
@@ -0,0 +1,26 @@
+package com.zetcode;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+// Sorting list of objects by their double fields
+
+record Company(String name, Double rating) {}
+
+public class ListSortDouble {
+
+    public static void main(String[] args) {
+
+        var companies = Arrays.asList(new Company("Comp A", 4.5),
+                new Company("Comp B", 6.5), new Company("Comp C", 3.1),
+                new Company("Comp D", 8.4), new Company("Comp E", 7.8));
+
+        companies.sort(Comparator.comparingDouble(Company::rating));
+        companies.forEach(System.out::println);
+
+        System.out.println("Reversed");
+
+        companies.sort(Comparator.comparingDouble(Company::rating).reversed());
+        companies.forEach(System.out::println);
+    }
+}
