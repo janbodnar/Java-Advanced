9030b04819d948801a1bcdcb2b92e3307b0b5b3a
Jan Bodnar <jan.bodnar@gmail.com>
2021-03-20 22:26:47 +0100

    Update ListSortEx2.java

--- comparesort/ListSortEx2.java
+++ comparesort/ListSortEx2.java
@@ -3,47 +3,11 @@ package com.zetcode;
 import java.util.Arrays;
 import java.util.Comparator;
 
-// Comparing objects by their fields
+// Sorting objects by their fields
 
-class Car {
+record Car(String name, int price) {}
 
-    private String name;
-    private int price;
-
-    public Car(String name, int price) {
-
-        this.name = name;
-        this.price = price;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
-    }
-
-    @Override
-    public String toString() {
-
-        final var sb = new StringBuilder("Car{");
-        sb.append("name='").append(name).append('\'');
-        sb.append(", price=").append(price);
-        sb.append('}');
-        return sb.toString();
-    }
-}
-
-public class ListSortEx2 {
+public class ListSortByFields {
 
     public static void main(String[] args) {
 
@@ -51,10 +15,10 @@ public class ListSortEx2 {
                 new Car("Mazda", 13700), new Car("Porsche", 353800),
                 new Car("Skoda", 8900),  new Car("Volkswagen", 19900));
 
-        cars.sort(Comparator.comparing(Car::getPrice));
+        cars.sort(Comparator.comparing(Car::price));
         System.out.println(cars);
 
-        cars.sort(Comparator.comparing(Car::getName));
+        cars.sort(Comparator.comparing(Car::name));
         System.out.println(cars);
     }
 }
