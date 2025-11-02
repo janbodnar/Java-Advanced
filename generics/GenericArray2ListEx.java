ecf2297d6620ae4e24c0bc3f6f05a8bf3a6bbdc4
Jan Bodnar <jan.bodnar@gmail.com>
2025-11-02 18:10:14 +0100

    Update GenericArray2ListEx.java

--- generics/GenericArray2ListEx.java
+++ generics/GenericArray2ListEx.java
@@ -1,29 +1,17 @@
-package com.zetcode;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.function.Function;
-import java.util.stream.Collectors;
+void main() {
 
-public class GenericArray2ListEx {
+  Integer[] vals = { 2, 4, 6, 8, 10, 12 };
+  Float[] vals2 = { 2.4f, 4.2f, 6.1f, 8.4f, 10.2f, 12.1f };
 
-    public static void main(String[] args) {
+  var myList = mapArrayToList(vals, e -> e * e);
+  System.out.println(myList);
 
-        Integer[] vals = {2, 4, 6, 8, 10, 12};
-        Float[] vals2 = {2.4f, 4.2f, 6.1f, 8.4f, 10.2f, 12.1f};
+  var myList2 = mapArrayToList(vals2, e -> e * e);
+  System.out.println(myList2);
+}
 
-        var myList = mapArrayToList(vals, e -> e * e);
-        System.out.println(myList);
+<T, R> List<R> mapArrayToList(T[] a, Function<T, R> mapFun) {
 
-        var myList2 = mapArrayToList(vals2, e -> e * e);
-        System.out.println(myList2);
-    }
-
-    private static <T, R> List<R> mapArrayToList(T[] a, Function<T, R> mapFun) {
-
-        return Arrays.stream(a)
-                .map(mapFun)
-                .collect(Collectors.toList());
-    }
-
+  return Arrays.stream(a).map(mapFun).collect(Collectors.toList());
 }
