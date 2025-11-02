70b1a957edcc0f4fd68f7da5ee00f8f654b4d40d
Jan Bodnar <jan.bodnar@gmail.com>
2025-11-02 18:08:46 +0100

    Update GenericConcatListEx.java

--- generics/GenericConcatListEx.java
+++ generics/GenericConcatListEx.java
@@ -1,37 +1,32 @@
-package com.zetcode;
 
-import java.util.ArrayList;
-import java.util.List;
+void main() {
 
-public class GenericConcatListEx {
+  var vals1 = List.of(1, 2, 3);
+  var vals2 = List.of(4, 5, 6);
+  var vals3 = List.of(7, 8, 9);
 
-    public static void main(String[] args) {
+  var vals = concatenate(vals1, vals2, vals3);
 
-        var vals1 = List.of(1, 2, 3);
-        var vals2 = List.of(4, 5, 6);
-        var vals3 = List.of(7, 8, 9);
+  IO.println(vals);
 
-        var vals = concatenate(vals1, vals2, vals3);
+  var words1 = List.of("pen", "paper", "ink");
+  var words2 = List.of("sky", "cloud", "wind");
+  var words3 = List.of("tree", "forest", "wood");
 
-        System.out.println(vals);
+  var words = concatenate(words1, words2, words3);
 
-        var words1 = List.of("pen", "paper", "ink");
-        var words2 = List.of("sky", "cloud", "wind");
-        var words3 = List.of("tree", "forest", "wood");
+  IO.println(words);
+}
 
-        var words = concatenate(words1, words2, words3);
+@SafeVarargs
+final <T> List<T> concatenate(List<T>... lists) {
+  return new ArrayList<>() {
+    {
 
-        System.out.println(words);
+      for (List<T> mylist : lists) {
+
+        addAll(mylist);
+      }
     }
-
-    @SafeVarargs
-    private static <T> List<T> concatenate(List<T>... lists) {
-        return new ArrayList<>() {{
-
-            for (List<T> mylist : lists) {
-
-                addAll(mylist);
-            }
-        }};
-    }
+  };
 }
