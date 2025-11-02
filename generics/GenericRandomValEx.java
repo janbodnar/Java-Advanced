a134b8e8ee122e2cc6275cdc5791731d8bb15dda
Jan Bodnar <jan.bodnar@gmail.com>
2025-11-02 16:18:19 +0100

    Update GenericRandomValEx.java

--- generics/GenericRandomValEx.java
+++ generics/GenericRandomValEx.java
@@ -1,26 +1,19 @@
-package com.zetcode;
 
-import java.util.List;
-import java.util.Random;
+void main() {
 
-public class GenericRandomValEx {
+  var words = List.of("rock", "sky", "blue", "ocean", "falcon");
+  var vals = List.of(2, 3, 4, 5, 6, 7, 8);
 
-    public static void main(String[] args) {
+  var e1 = getRandomElement(words);
+  IO.println(e1);
 
-        var words = List.of("rock", "sky", "blue", "ocean", "falcon");
-        var vals = List.of(2, 3, 4, 5, 6, 7, 8);
+  var e2 = getRandomElement(vals);
+  IO.println(e2);
+}
 
-        var e1 = getRandomElement(words);
-        System.out.println(e1);
+<T> T getRandomElement(List<T> list) {
 
-        var e2 = getRandomElement(vals);
-        System.out.println(e2);
-    }
-
-    private static <T> T getRandomElement(List<T> list) {
-
-        var random = new Random();
-        int idx = random.nextInt(list.size());
-        return list.get(idx);
-    }
+  var random = new Random();
+  int idx = random.nextInt(list.size());
+  return list.get(idx);
 }
