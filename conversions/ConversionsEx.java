e6b5d50fe64b898bf2eb488f2e77384857e20b73
Jan Bodnar <jan.bodnar@gmail.com>
2025-11-03 10:56:12 +0100

    Update ConversionsEx.java

--- conversions/ConversionsEx.java
+++ conversions/ConversionsEx.java
@@ -1,53 +1,45 @@
-package com.zetcode;
 
-import java.util.Objects;
+void main() {
 
-public class ConversionsEx {
 
-    public static void main(String[] args) {
-        
-        // https://stackoverflow.com/questions/1514910/how-to-properly-compare-two-integers-in-java?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+  Integer m = 4;
+  Integer n = 5;
 
-        Integer m = 4;
-        Integer n = 5;
-        
-        if (Objects.equals(m, n)) {
-            
-            System.out.println("Variables are equal");
-        } else {
-            System.out.println("Variables are not equal");
-        }
-        
-        if (m.intValue() == n.intValue()) {
-            
-            System.out.println("Variables are equal");
-        } else {
-            System.out.println("Variables are not equal");
-        }
-        
+  if (Objects.equals(m, n)) {
 
-        // you can't convert-and-autobox in one step
-        // you can't implicitly narrow a type
-        // 1L
-        Long num = 1L;
+    System.out.println("Variables are equal");
+  } else {
+    System.out.println("Variables are not equal");
+  }
 
-        // Why new Long(10) compiles? Because there is a suitable constructor 
-        // available that excepts integer arguments. 
-        Long num2 = new Long(2);
+  if (m.intValue() == n.intValue()) {
 
-        Byte b = new Byte((byte) 10);
-        byte b2 = 34;
+    System.out.println("Variables are equal");
+  } else {
+    System.out.println("Variables are not equal");
+  }
 
-        Long x = (long) 10;
+  // you can't convert-and-autobox in one step
+  // you can't implicitly narrow a type
+  // 1L
+  Long num = 1L;
 
-        int x2 = (int) 23.4;
-        double x3 = 23;
+  Long num2 = Long.valueOf(2);
 
-        double x4 = 32f;
-        float x5 = (float) 35.567d;
+  Byte b = Byte.valueOf((byte) 10);
+  byte b2 = 34;
 
-        byte g = (12) * 3;
-        //byte g2 = 3 * g; 
-    }
+  Long x = (long) 10;
 
+  int x2 = (int) 23.4;
+  double x3 = 23;
+
+  double x4 = 32f;
+  float x5 = (float) 35.567d;
+
+  byte g = (12) * 3;
+  // byte g2 = 3 * g;
 }
+
+
+// https://stackoverflow.com/questions/1514910/how-to-properly-compare-two-integers-in-java?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
