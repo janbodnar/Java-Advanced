311b078826b0617afd0b8fc63a4c2439de986bf0
Jan Bodnar <jan.bodnar@gmail.com>
2025-11-03 10:43:37 +0100

    Update PropsEnv.java

--- PropsEnv.java
+++ PropsEnv.java
@@ -7,17 +7,17 @@ void main() {
 
   for (var directory : directories) {
 
-    System.out.println(directory);
+    IO.println(directory);
   }
 
-  System.out.printf("There are %d items in the PATH variable%n",
-      directories.length);
+  IO.println(String.format("There are %d items in the PATH variable%n",
+      directories.length));
 
   String os_version = System.getProperty("os.name");
   String java_version = System.getProperty("java.version");
   String java_home = System.getProperty("java.home");
 
-  System.out.println(os_version);
-  System.out.println(java_version);
-  System.out.println(java_home);
+  IO.println(os_version);
+  IO.println(java_version);
+  IO.println(java_home);
 }
