d05bf3c3cadd4281217037eb6e50d88571c56f5d
Jan Bodnar <jan.bodnar@gmail.com>
2025-12-08 11:27:33 +0100

    Refactor IncrementOperator class main method
    Refactor IncrementOperator class to use IO for output and simplify main method.

--- pitfalls/IncrementOperator.java
+++ pitfalls/IncrementOperator.java
@@ -1,20 +1,15 @@
-package com.zetcode;
-
 // there is a subtle difference between the 
 // prefix and suffix increment operators
 
-public class IncrementOperator {
+void main() {
 
-    public static void main(String[] args) {
+    int num = 0;
 
-        int num = 0;
+    num = num++;
 
-        num = num++;
+    IO.println(num);
 
-        System.out.println(num);
-
-        num = ++num;
-        
-        System.out.println(num);
-    }
+    num = ++num;
+    
+    IO.println(num);
 }
