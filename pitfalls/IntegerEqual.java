53713ee6773e95b9eb9d8c3adac356d7bdce48ea
Jan Bodnar <jan.bodnar@gmail.com>
2025-12-08 11:30:04 +0100

    Refactor IntegerEqual class main method
    Refactored main method and updated print statements to use IO class.

--- pitfalls/IntegerEqual.java
+++ pitfalls/IntegerEqual.java
@@ -1,20 +1,16 @@
-package com.zetcode;
-
 // The == operator compares object references and equals()
 // object values. Java internally caches integers 
 // from -128 to +127, so comparing them with == 'works'.
 // we should use equals
 
-public class IntegerEqual {
+void main() {
 
-    public static void main(String[] args) {
+    Integer a = 127;
+    Integer b = 127;
+    Integer c = 128;
+    Integer d = 128;
 
-        Integer a = 127;
-        Integer b = 127;
-        Integer c = 128;
-        Integer d = 128;
-
-        System.out.println(a == b);
-        System.out.println(c == d);
-    }
+    IO.println(a == b);
+    IO.println(c == d);
 }
+
